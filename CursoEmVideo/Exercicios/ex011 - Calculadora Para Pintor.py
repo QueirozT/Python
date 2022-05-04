054902d8edb8f78c79b06ddb0bd0659bfe0643e9
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 17:22:22 -0300

    Create ex011 - Calculadora Para Pintor.py

---
+++ CursoEmVideo/Exercicios/ex011 - Calculadora Para Pintor.py
@@ -0,0 +1,10 @@
+print('===== EXERCÍCIO 011 =====')
+print()
+
+altura = float(input('Qual a Altura? '))
+largura = float(input('Qual a Largura? '))
+print()
+
+print('A área da parede é de {:.2f}m².'.format(altura * largura))
+print('Você precisará de {:.2f}L de tinta.'.format(altura * largura / 2))
+print()
