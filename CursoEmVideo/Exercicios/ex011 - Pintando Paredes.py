ec366f3a4d0fbeedfeb60d8995db01089ef1b69b
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 20:21:13 -0300

    Alterando Descrição do Exercício

--- CursoEmVideo/Exercicios/ex011 - Pintando Paredes.py
+++ CursoEmVideo/Exercicios/ex011 - Pintando Paredes.py
@@ -5,6 +5,6 @@ altura = float(input('Qual a Altura? '))
 largura = float(input('Qual a Largura? '))
 print()
 
-print('A área da parede é de {:.2f}m².'.format(altura * largura))
-print('Você precisará de {:.2f}L de tinta.'.format(altura * largura / 2))
+print('A área da parede é de {:.1f}m².'.format(altura * largura))
+print('Você precisará de {:.1f}L de tinta.'.format(altura * largura / 2))
 print()
