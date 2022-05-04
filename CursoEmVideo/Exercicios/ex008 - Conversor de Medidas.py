55a2fa7927bd83eb20a4064ebfc0668d5f743ac7
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 16:55:27 -0300

    Create ex008 - Conversor de Medidas.py

---
+++ CursoEmVideo/Exercicios/ex008 - Conversor de Medidas.py
@@ -0,0 +1,7 @@
+print('===== EXERCÍCIO 008 =====')
+print()
+
+valor = int(input('Digite um Valor em Metros: '))
+print()
+
+print('{}m é equivalente a {}cm ou {}mm'.format(valor, valor * 100, valor * 1000))
