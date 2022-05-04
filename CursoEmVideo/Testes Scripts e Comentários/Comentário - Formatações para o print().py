f3bcb72bc6975fb4552f856b2b661204eb52aa11
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 16:10:24 -0300

    Update Comentário - Formatações para o print().py

--- CursoEmVideo/Testes Scripts e Comentários/Comentário - Formatações para o print().py
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Formatações para o print().py
@@ -1,9 +1,20 @@
-altura = 1.7345 # O valor em ponto flutuante será delimitado pela formatação {:.2f}
+# Para quebrar linhas, use o \n em qualquer lugar do print()
 
-nome = 'Pedro' # O nome receberá espaços extras caso a quantidade de caracteres seja menor que a da formatação {:^20} e receberá '=' para preecher os espaços vazios.
+# Para não quebrar linhas, use o end='' no final do print()
 
+
+# Para delimitar o tamanho de um float, dentro das chaves de formatação utilize {:.2f}
+
+# Para adicionar espaços extras contando a quantidade de caracteres da string, utilize {:^10}. para alinhar o texto a esquerda utilize {:<10} e para alinhar a direita utilize {:>10}. Para adicionar caracteres especiais, utilize {:=^10}
+
+# EX:
+
+altura = 1.7345
+nome = 'Pedro'
 peso = '60kg'
 
-print('O {:=^20} tem {:.2f} e '.format(nome, altura), end='') # O próximo print() será concatenado ao final graças ao end='' que remove quebras de linha.
+print(' O \n {:=^20} \n tem {:.2f} de altura e '.format(nome, altura), end='')
 
 print('{}'.format(peso))
+
+
