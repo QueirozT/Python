d24e36c53d76be16e93f6398a8ab90792c7af201
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 16:30:38 -0300

    Create ex005 - Sucessor e Predecessor.py

---
+++ CursoEmVideo/Exercicios/ex005 - Sucessor e Predecessor.py
@@ -0,0 +1,7 @@
+print('===== EXERCÍCIO 005 =====')
+print()
+
+valor = int(input('Digite um Número: '))
+print()
+
+print('O Predecessor de {} é: {} e o Sucessor é: {}'.format(valor, valor - 1, valor + 1))
