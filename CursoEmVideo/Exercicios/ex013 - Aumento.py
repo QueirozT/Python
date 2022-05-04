e4baa253daebce90afe619e62aad05b8a04db54e
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 17:31:27 -0300

    Create ex013 - Aumento.py

---
+++ CursoEmVideo/Exercicios/ex013 - Aumento.py
@@ -0,0 +1,8 @@
+print('===== EXERCÍCIO 013 =====')
+print()
+
+valor = float(input('Qual o valor do seu salário? '))
+print()
+
+print('Seu salário é R$ {:.2f} Com o aumento de 15%, o seu salário será R$ {:.2f}'.format(valor, valor + (valor * 0.15)))
+print()
