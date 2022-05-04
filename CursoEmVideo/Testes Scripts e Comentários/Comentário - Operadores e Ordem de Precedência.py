45abff64ac5773a1864318af117ee6de5f2580e1
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 15:30:08 -0300

    Create Comentário - Operadores e Ordem de Precedência.py

---
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Operadores e Ordem de Precedência.py
@@ -0,0 +1,34 @@
+# Operador de Adição: +
+# Operador de Subtração: -
+# Operador de Multiplicação: *
+# Operador de Divisão: /
+
+# Operador de Exponenciação: **
+# Operador de Divisão Inteira: //
+# Operador de Resto da Divisão: %
+
+# Operador de Atribuição: =
+# Operador de Concatenação: +
+# Operador de Comparação: ==
+# Operador de Desigualdade: !=
+
+# Operador de Maior que: >
+# Operador de Menor que: <
+# Operador de Maior ou Igual que: >=
+# Operador de Menor ou Igual que: <=
+
+# Operador Lógico E: and
+# Operador Lógico OU: or
+# Operador Lógico NÃO: not
+
+#=======================================================================================================================
+
+#Ordem de precedência:
+# ()
+# **
+# * / // %
+# + -
+# > < >= <=
+# == !=
+# and or
+# not
