0ce0ec3cfc3d9e750647d7df23d1992536fc9254
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 17:12:19 -0300

    Create ex010 - Conversor de Moeda.py

---
+++ CursoEmVideo/Exercicios/ex010 - Conversor de Moeda.py
@@ -0,0 +1,7 @@
+print('===== EXERCÍCIO 010 =====')
+print()
+
+valor = float(input('Quantos reais você tem? '))
+print()
+
+print('Você tem {} reais, o valor do dólar é {}. você pode comprar {:.2f} dólares.'.format(valor, 4.91, valor / 4.91))
