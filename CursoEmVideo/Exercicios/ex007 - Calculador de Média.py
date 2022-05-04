82904e174ea8393b14be7bdb515aa925be30cade
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 16:41:08 -0300

    Create ex007 - Calculador de Média.py

---
+++ CursoEmVideo/Exercicios/ex007 - Calculador de Média.py
@@ -0,0 +1,8 @@
+print('===== EXERCÍCIO 007 =====')
+print()
+
+primeiro = float(input('Primeira Nota: '))
+segundo = float(input('Segunda Nota: '))
+print()
+
+print('A média das notas é: {}'.format((primeiro + segundo) / 2))
