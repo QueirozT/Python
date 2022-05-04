8cc94d0023b9959caf2532755d3364cce00ce373
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 19:49:29 -0300

    Update ex008 - Conversor de Medidas.py

--- CursoEmVideo/Exercicios/ex008 - Conversor de Medidas.py
+++ CursoEmVideo/Exercicios/ex008 - Conversor de Medidas.py
@@ -1,7 +1,7 @@
 print('===== EXERCÍCIO 008 =====')
 print()
 
-valor = int(input('Digite um Valor em Metros: '))
+valor = int(input('Digite uma distância em metros: '))
 print()
 
 print('{}m é equivalente a {}cm ou {}mm'.format(valor, valor * 100, valor * 1000))
