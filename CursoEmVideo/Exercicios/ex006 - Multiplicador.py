ea9b55bb5c23bd740eee726033e7baa8e6cc167a
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 19:15:30 -0300

    Update ex006 - Multiplicador.py

--- CursoEmVideo/Exercicios/ex006 - Multiplicador.py
+++ CursoEmVideo/Exercicios/ex006 - Multiplicador.py
@@ -1,7 +1,7 @@
 print('===== EXERCÍCIO 006 =====')
 print()
 
-valor = int(input('Digite um Número: '))
+valor = float(input('Digite um Número: '))
 print()
 
-print('O dobro de {} é: {} \nO triplo de {} é: {} \nA raiz quadrada de {} é: {}'.format(valor, valor * 2, valor, valor * 3, valor, valor ** 2))
+print('O dobro de {:.2f} é: {:.2f} \nO triplo de {:.2f} é: {:.2f} \nA raiz quadrada de {:.2f} é: {:.2f}'.format(valor, valor * 2, valor, valor * 3, valor, valor ** (1/2)))
