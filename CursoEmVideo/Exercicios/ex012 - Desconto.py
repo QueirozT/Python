906d1366d709fdda142d2f28fa6e255965f99459
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 17:27:25 -0300

    Create ex012 - Desconto.py

---
+++ CursoEmVideo/Exercicios/ex012 - Desconto.py
@@ -0,0 +1,8 @@
+print('===== EXERCÍCIO 012 =====')
+print()
+
+valor = float(input('Qual o valor do produto? '))
+print()
+
+print('O valor do produto é R$ {:.2f} O Desconto é de 5%, o Valor do produto com desconto é R$ {:.2f}'.format(valor, valor - (valor * 0.05)))
+print()
