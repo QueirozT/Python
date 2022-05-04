12d800fe130c8bd3b25fca725b05edb883d2385f
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 17:16:11 -0300

    Update ex010 - Conversor de Moeda.py

--- CursoEmVideo/Exercicios/ex010 - Conversor de Moeda.py
+++ CursoEmVideo/Exercicios/ex010 - Conversor de Moeda.py
@@ -4,4 +4,4 @@ print()
 valor = float(input('Quantos reais você tem? '))
 print()
 
-print('Você tem {} reais, o valor do dólar é {}. você pode comprar {:.2f} dólares.'.format(valor, 4.91, valor / 4.91))
+print('Você tem R$ {:.2f} Reais, o valor do dólar atual é US$ {:.2f} você pode comprar US$ {:.2f} Dólares.'.format(valor, 4.91, valor / 4.91))
