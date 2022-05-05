54724a8d42e65dfbc09837423d1a3db00bc360da
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-05 07:13:24 -0300

    Create ex016 -  Quebrando um Número.py

---
+++ CursoEmVideo/Exercicios/ex016 -  Quebrando um Número.py
@@ -0,0 +1,7 @@
+print('===== EXERCÍCIO 016 =====')
+print()
+
+numero = float(input('Digite um número: '))
+print()
+
+print('O número digitado foi {}, sua porção inteira é {}.'.format(numero, int(numero)))
