964df32560216fc2c4cef58a402c7d284432fd5a
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-05 07:19:03 -0300

    Update ex016 -  Quebrando um Número.py

--- CursoEmVideo/Exercicios/ex016 -  Quebrando um Número.py
+++ CursoEmVideo/Exercicios/ex016 -  Quebrando um Número.py
@@ -1,3 +1,7 @@
+# import math
+
+# from math import trunc
+
 print('===== EXERCÍCIO 016 =====')
 print()
 
@@ -5,3 +9,9 @@ numero = float(input('Digite um número: '))
 print()
 
 print('O número digitado foi {}, sua porção inteira é {}.'.format(numero, int(numero)))
+
+# print('O número digitado foi {}, sua porção inteira é {:.0f}.'.format(numero, numero))
+
+# print('O número digitado foi {}, sua porção inteira é {}.'.format(numero, math.trunc(numero)))
+
+# print('O número digitado foi {}, sua porção inteira é {}.'.format(numero, trunc(numero)))
