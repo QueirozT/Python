4491e4c00944b3fa5bb913bd2f8fff1dbebcc055
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 21:28:52 -0300

    Create ex014 - Conversor de Temperatura.py

---
+++ CursoEmVideo/Exercicios/ex014 - Conversor de Temperatura.py
@@ -0,0 +1,7 @@
+print('===== EXERCÍCIO 014 =====')
+print()
+
+valor = float(input('Qual a temperatura em °C? '))
+print()
+
+print('A temperatuda de {}°C corresponde a {}°F'.format(valor, 9 * valor / 5 + 32)) # Macete para converter de C para F (C * 1.8 + 32)
