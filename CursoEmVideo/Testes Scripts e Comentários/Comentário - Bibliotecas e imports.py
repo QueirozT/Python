c46d0451d45cd300aa6865e0ccdbd05605b0a9be
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-04 22:39:06 -0300

    Create Comentário - Bibliotecas e imports.py

---
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Bibliotecas e imports.py
@@ -0,0 +1,8 @@
+# Os Módulos ou Bibliotecas servem para adicionar funcionalidades extras ao Python. 
+# Para adicionar uma biblioteca ou módulo ao Python, é necessário instalá-lo no sistema utilizando o comando "pip install nome".
+
+# import biblioteca - Usado para importar uma biblioteca e utilizar seus comandos.
+# from biblioteca import comando - Usado para importar comandos específicos de bibliotecas.
+
+# Para visualizar algumas bibliotecas do python pre-instaladas, basta digitar 'import' e apertar o atalho de visualização "ctrl + space"
+# Uma lista com todas as bibliotecas de python pode ser vista em Pypi: https://pypi.org
