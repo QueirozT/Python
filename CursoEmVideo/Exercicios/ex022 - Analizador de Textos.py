76f09c139e8577a7d936f6a91ea80d3c803afa1e
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-06 16:23:08 -0300

    Update ex022 - Analizador de Textos.py
    Adicionando Resposta alternativa para calcular a quantidade de caracteres da primeira palavra separada por espaço.

--- CursoEmVideo/Exercicios/ex022 - Analizador de Textos.py
+++ CursoEmVideo/Exercicios/ex022 - Analizador de Textos.py
@@ -8,8 +8,9 @@ print('Analizando seu nome...')
 print('Seu nome em maiscula é: {}'.format(nome.upper()))
 print('Seu nome em minuscula é: {}'.format(nome.lower()))
 print('Seu nome ao todo tem {} letras.'.format(len(''.join(nome.split()))))
-print('Seu primeiro nome é {} e ele tem {} letras.'.format(nome.split()[0],len(nome.split()[0])))
+print('Seu primeiro nome é "{}" e ele tem {} letras.'.format(nome.split()[0],len(nome.split()[0])))
 
 # Resultado Alternativo:
 
 # print('Seu nome ao todo tem {} letras.'.format(len(nome) - nome.count(' ')))
+# print('Seu primeiro nome é {} e ele tem {} letras.'.format(nome.split()[0], nome.find(' ')))
