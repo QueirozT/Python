166b7d246645bc9c5fcffb8d0bcaa314b5b379db
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-06 15:41:44 -0300

    Create ex025 - Procurando uma String dentro de Outra.py

---
+++ CursoEmVideo/Exercicios/ex025 - Procurando uma String dentro de Outra.py
@@ -0,0 +1,11 @@
+print('===== Exercício 25 =====')
+print()
+
+nome = input('Digite o seu nome: ')
+print()
+
+if 'SILVA' in nome.upper():
+    print('O nome "{}" contém a palavra "Silva"'.format(nome))
+else:
+    print('O nome "{}" não contém a palavra "Silva"'.format(nome))
+    print()
