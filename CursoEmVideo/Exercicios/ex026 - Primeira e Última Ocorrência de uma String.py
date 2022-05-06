1c679cb79b1daed11a35abc1affc8f4f4b23ac6c
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-06 17:36:26 -0300

    Update ex026 - Primeira e Última Ocorrência de uma String.py

--- CursoEmVideo/Exercicios/ex026 - Primeira e Última Ocorrência de uma String.py
+++ CursoEmVideo/Exercicios/ex026 - Primeira e Última Ocorrência de uma String.py
@@ -1,14 +1,14 @@
 print('===== Exercício 26 =====')
 print()
 
-valor = input('Digite uma frase: ')
+valor = str(input('Digite uma frase: ')).strip()
 print()
 
 print('A letra "A" aparece {} vezes na frase.'.format(valor.upper().count('A')))
 print()
 
-print('A primeira letra "A" aparece na posição {}.'.format(valor.upper().find('A')))
+print('A primeira letra "A" aparece na posição {}.'.format(valor.upper().find('A') + 1))
 print()
 
-print('A última letra "A" aparece na posição {}.'.format(valor.upper().rfind('A')))
+print('A última letra "A" aparece na posição {}.'.format(valor.upper().rfind('A') + 1))
 print()
