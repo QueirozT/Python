e68c8ed3e89d0c81e2032f8fcdb6a0b73b697a2f
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-06 16:17:29 -0300

    Update ex022 - Analizador de Textos.py
    Adicionado o .strip() para remoção de espaços desnecessários e uma resposta alternativa para calcular espaços vazios.

--- CursoEmVideo/Exercicios/ex022 - Analizador de Textos.py
+++ CursoEmVideo/Exercicios/ex022 - Analizador de Textos.py
@@ -1,7 +1,7 @@
 print('====== Exercício 22 ======')
 print()
 
-nome = input('Digite o seu nome: ')
+nome = str(input('Digite o seu nome: ')).strip()
 print()
 
 print('Analizando seu nome...')
@@ -9,3 +9,7 @@ print('Seu nome em maiscula é: {}'.format(nome.upper()))
 print('Seu nome em minuscula é: {}'.format(nome.lower()))
 print('Seu nome ao todo tem {} letras.'.format(len(''.join(nome.split()))))
 print('Seu primeiro nome é {} e ele tem {} letras.'.format(nome.split()[0],len(nome.split()[0])))
+
+# Resultado Alternativo:
+
+# print('Seu nome ao todo tem {} letras.'.format(len(nome) - nome.count(' ')))
