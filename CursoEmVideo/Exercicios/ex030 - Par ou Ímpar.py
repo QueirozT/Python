9bdb64f9eae5b55432f48580a2a8dc7033ae5571
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-07 13:36:05 -0300

    Create ex030 - Par ou Ímpar.py

---
+++ CursoEmVideo/Exercicios/ex030 - Par ou Ímpar.py
@@ -0,0 +1,11 @@
+print('===== EXERCÍCIO 30 =====')
+print()
+
+valor = input('Digite um valor: ')
+print()
+
+if (int(valor) % 2) == 0:
+    print('"{}" é par!'.format(valor))
+else:
+    print('"{}" é ímpar!'.format(valor))
+print()
