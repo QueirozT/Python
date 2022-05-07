67073d30aaf775fa7c8da184b6f1672ebb2d495c
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-06 23:54:04 -0300

    Create Comentário - Estruturas Condicionais.py

---
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Estruturas Condicionais.py
@@ -0,0 +1,37 @@
+# CONDICIONAL SIMPLES
+
+if True:
+    print('Verdadeiro')
+else:
+    print('Falso')
+
+
+# CONDICIONAL COM DOIS OPERADORES
+
+if True and False:
+    print('Verdadeiro')
+else:
+    print('Falso')
+
+
+# CONDICIONAL COM TRES OPERADORES
+
+if True and False or True:
+    print('Verdadeiro')
+else:
+    print('Falso')
+
+# CONDICIONAL SIMPLIFICADA
+
+print('Verdadeiro' if True else 'Falso')
+
+
+# CONDICIONAL COMPLEXA
+
+if True:
+    print('Verdadeiro')
+elif False:
+    print('Falso')
+else:
+    print('Não sei')
+
