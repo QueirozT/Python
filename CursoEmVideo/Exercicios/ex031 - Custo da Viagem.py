a98c357380fdcbe11e6e12ec707e29518443b313
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-07 14:02:05 -0300

    Create ex031 - Custo da Viagem.py

---
+++ CursoEmVideo/Exercicios/ex031 - Custo da Viagem.py
@@ -0,0 +1,18 @@
+print('===== EXERCÍCIO 31 =====')
+print()
+
+valor = input('Qual a distância da viagem em km? ')
+print()
+
+if valor.isnumeric():
+    valor = float(valor)
+
+    if valor >= 200:
+        print('A sua passagem custará R${:.2f}'.format(valor * 0.45))
+        print()
+    else:
+        print('A sua passagem custará R${:.2f}'.format(valor * 0.50))
+        print()
+else:
+    print('"{}" é um valor inválido!'.format(valor))
+    print()
