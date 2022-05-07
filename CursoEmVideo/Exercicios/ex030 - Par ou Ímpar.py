1637b814ea086a1e1f31f9b0a72e186f28c63a34
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-07 13:52:46 -0300

    Update ex030 - Par ou Ímpar.py

--- CursoEmVideo/Exercicios/ex030 - Par ou Ímpar.py
+++ CursoEmVideo/Exercicios/ex030 - Par ou Ímpar.py
@@ -4,8 +4,17 @@ print()
 valor = input('Digite um valor: ')
 print()
 
-if (int(valor) % 2) == 0:
-    print('"{}" é par!'.format(valor))
+if valor.isdecimal():
+    if int(valor) % 2 == 0:
+        print('"{}" é par!'.format(valor))
+    else:
+        print('"{}" é ímpar!'.format(valor))
+    print()
+elif valor.isalpha() or valor.isalnum():
+    print('Para funcionar, precisamos de um número. "{}" não é um número!'.format(valor))
+    print()
 else:
-    print('"{}" é ímpar!'.format(valor))
-print()
+    print('Não é possível determinar se "{}" é Par ou Ímpar!'.format(valor))
+    print('Digite um valor acima de 0 sem ponto flutuante!')
+    print()
+
