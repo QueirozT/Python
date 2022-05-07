456981f737bf87bd0e7a25b8490cad1ec2b00606
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-07 17:06:54 -0300

    Create ex035 - Analizando Triângulo v1.0.py

---
+++ CursoEmVideo/Exercicios/ex035 - Analizando Triângulo v1.0.py
@@ -0,0 +1,14 @@
+print('===== EXERCÍCIO 35 =====')
+print()
+
+v01 = float(input('Qual o comprimento da primeira reta? '))
+v02 = float(input('Qual o comprimento da segunda reta? '))
+v03 = float(input('Qual o comprimento da terceira reta? '))
+print()
+
+if (v01 + v02) > v03 and (v01 + v03) > v02 and (v02 + v03) > v01:
+    print('Um triângulo pode ser feito com as medidas fornecidas.')
+    print()
+else:
+    print('Não é possível fazer um triângulo com estas medidas.')
+    print()
