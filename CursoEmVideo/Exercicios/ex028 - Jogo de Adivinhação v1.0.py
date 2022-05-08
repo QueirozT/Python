7bd400bd6f78cd2942d07ce3fbb6cf2354405cf3
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-07 22:23:27 -0300

    Update ex028 - Jogo de Adivinhação v1.0.py
    Atualizando as interações do programa e utilizando mais uma funcionalidade do random e uma funcionalidade da biblioteca time

--- CursoEmVideo/Exercicios/ex028 - Jogo de Adivinhação v1.0.py
+++ CursoEmVideo/Exercicios/ex028 - Jogo de Adivinhação v1.0.py
@@ -1,28 +1,61 @@
-from random import randint
+from random import randint, choice
+from time import sleep
 
 print('===== EXERCÍCIO 28 =====')
 print()
 
-number = randint(0, 5) # Variável que recebe um número aleatório entre 0 e 5
+number = randint(0, 5) # Variável que recebe um número aleatório entre 0 e 5.
 
+# Primeira interação do programa.
 print('-=-' * 20)
 print('Estou pensando em um número entre 0 e 5. tente adivinhar...')
 print('-=-' * 20)
 print()
 
+# Coleta do número do usuário.
 value = input('Em que número eu pensei? ')
 print()
 
+# mensagem de espera.
+print('Só um segundo...')
+print()
+sleep(3)
+
+# Mensagens alternativas para interação com o usuário.
+memoria = choice(['a', 'b', 'c', 'd'])
+
+if memoria == 'a' or memoria == 'd':
+    print('O que eu est...')
+    print()
+    sleep(4)
+
+    print('AHHHH LEMBREI!')
+    print()
+    sleep(2)
+
+    print('Desculpe por isso, memória cheia sabe...')
+    print()
+    sleep(2)
+elif memoria == 'b' or memoria == 'c':
+    print('Desculpe pela demora...')
+    print()
+    sleep(2)
+
+    print('Estava limpando o cachê :)')
+    print()
+    sleep(2)
+
+# Resultado e ultima interação do programa.
 if value.isnumeric():
     value = int(value)
 
     if value == number:
-        print('Você é muito bom! eu estava mesmo pensando em "{}"'.format(number))
+        print('Parabéns! eu estava mesmo pensando em "{}"'.format(number))
     elif value > 5:
         print('Você não entendeu as regras. "{}" não está entre 0 e 5.'.format(value))
     else:
         print('Você errou! eu estava pensando em "{}"'.format(number))
     print()
 else:
-    print('Você é burro? "{}" não é um número...'.format(value))
+    print('Você é burro? "{}" não é um número.'.format(value))
     print()
