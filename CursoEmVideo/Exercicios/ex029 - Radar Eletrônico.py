c14a5ea477718693fa99abe27bab891afbc88872
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-07 22:31:50 -0300

    Update ex029 - Radar Eletrônico.py
    Alteradas as descrições de interação

--- CursoEmVideo/Exercicios/ex029 - Radar Eletrônico.py
+++ CursoEmVideo/Exercicios/ex029 - Radar Eletrônico.py
@@ -1,14 +1,13 @@
 print('===== EXERCÍCIO 29 =====')
 print()
 
-print('Esta é uma via de 80km/h.')
 valor = float(input('A quantos km/h você está? '))
 print()
 
 if valor > 80:
-    print('Você está acima da velocidade!')
-    print('Uma multa no valor de R${:.2f} foi gerada.'.format((valor - 80) * 7))
+    print('MULTADO! Você está acima do limite de velocidade que é 80km/h')
+    print('Você deve pagar a multa no valor de R${:.2f}'.format((valor - 80) * 7))
     print('Dirija com cuidado!')
 else:
-    print('Parabéns, você está dentro da velocidade!')
+    print('Tenha um bom dia! Dirija com segurança!')
 print()
