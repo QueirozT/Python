bc02df9a152933408bc46c953a347ed324a6c1d4
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-07 21:53:08 -0300

    Update ex028 - Jogo de Adivinhação v1.0.py
    Atualizando algumas interações do programa.

--- CursoEmVideo/Exercicios/ex028 - Jogo de Adivinhação v1.0.py
+++ CursoEmVideo/Exercicios/ex028 - Jogo de Adivinhação v1.0.py
@@ -5,20 +5,24 @@ print()
 
 number = randint(0, 5) # Variável que recebe um número aleatório entre 0 e 5
 
-print('Este é um jogo de adivinhação.')
-value = input('Estou pensando em um número entre 0 e 5, qual você acha que é? ')
+print('-=-' * 20)
+print('Estou pensando em um número entre 0 e 5. tente adivinhar...')
+print('-=-' * 20)
+print()
+
+value = input('Em que número eu pensei? ')
 print()
 
 if value.isnumeric():
     value = int(value)
 
     if value == number:
-        print('Parabéns, você é muito bom! eu estava mesmo pensando em "{}"'.format(number))
+        print('Você é muito bom! eu estava mesmo pensando em "{}"'.format(number))
     elif value > 5:
-        print('Você não entendeu as regras, "{}" não está entre 0 e 5.'.format(value))
+        print('Você não entendeu as regras. "{}" não está entre 0 e 5.'.format(value))
     else:
-        print('Sinto muito, você errou! eu estava pensando em "{}"'.format(number))
+        print('Você errou! eu estava pensando em "{}"'.format(number))
     print()
 else:
-    print('"{}" não é um número, não quer brincar comigo? T-T'.format(value))
+    print('Você é burro? "{}" não é um número...'.format(value))
     print()
