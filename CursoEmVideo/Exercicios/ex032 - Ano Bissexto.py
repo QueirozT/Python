76cef6472cb0c6d6c971558f5c78709bfc5fefff
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-08 13:56:30 -0300

    Update ex032 - Ano Bissexto.py

--- CursoEmVideo/Exercicios/ex032 - Ano Bissexto.py
+++ CursoEmVideo/Exercicios/ex032 - Ano Bissexto.py
@@ -4,8 +4,9 @@ print()
 valor = input('Digite o ano que você deseja saber se é bissexto: ')
 print()
 
+# Se o valor for divisível por 4 e (não for divisível por 100 /ou for divisível 400), significa que é bissexto!
 if valor.isnumeric():
-    if int(valor) % 4 == 0: # Se o resto da divisão por 4 for 0, o ano é bissexto.
+    if int(valor) % 4 == 0 and int(valor) % 100 != 0 or int(valor) % 400 == 0:
         print('O ano "{}" é bissexto!'.format(valor))
         print()
     else:
