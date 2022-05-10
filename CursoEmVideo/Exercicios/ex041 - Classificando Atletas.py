63654968e13130a0a9f13cee913bebeff6508193
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-10 13:52:19 -0300

    Create ex041 - Classificando Atletas.py

---
+++ CursoEmVideo/Exercicios/ex041 - Classificando Atletas.py
@@ -0,0 +1,21 @@
+print('===== EXERCÍCIO #41 =====')
+print()
+
+idade = int(input('Qual sua idade? '))
+print()
+
+if idade <= 9:
+    print('Sua classificação é "MIRIM"')
+    print()
+elif idade <= 14:
+    print('Sua classificação é "INFANTIL"')
+    print()
+elif idade <= 19:
+    print('Sua classificação é "JUNIOR"')
+    print()
+elif idade <= 20:
+    print('Sua classificação é "SÊNIOR"')
+    print()
+else:
+    print('Sua classificação é "MASTER"')
+    print()
