981df7df29ac707e12a9706a5da2da202e6c86d4
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-09 23:00:03 -0300

    Update ex040 - Calculando Média.py
    Adicionando cores para destacar a mensagem.

--- CursoEmVideo/Exercicios/ex040 - Calculando Média.py
+++ CursoEmVideo/Exercicios/ex040 - Calculando Média.py
@@ -8,11 +8,11 @@ print()
 media = (nota01 + nota02) / 2
 
 if media < 5:
-    print('Sua média é: {:.1f} \nVocê está REPROVADO!'.format(media))
+    print('Sua média é: {:.1f} \nVocê está {}REPROVADO!{}'.format(media, '\033[31m', '\033[m'))
     print()
 elif media < 6.9:
-    print('Sua média é: {:.1f} \nVocê está de RECUPERAÇÃO!'.format(media))
+    print('Sua média é: {:.1f} \nVocê está de {}RECUPERAÇÃO!{}'.format(media, '\033[34m', '\033[m'))
     print()
 else:
-    print('Sua média é: {:.1f} \nVocê está APROVADO!'.format(media))
+    print('Sua média é: {:.1f} \nVocê está {}APROVADO!{}'.format(media, '\033[32m', '\033[m'))
     print()
