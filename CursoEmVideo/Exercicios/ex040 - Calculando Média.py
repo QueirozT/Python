03205241252da2e2d58a2099cba3a6697a018f4d
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-11 20:06:38 -0300

    Update ex040 - Calculando Média.py

--- CursoEmVideo/Exercicios/ex040 - Calculando Média.py
+++ CursoEmVideo/Exercicios/ex040 - Calculando Média.py
@@ -11,7 +11,7 @@ if media < 5:
     print('Com {:.1f} e {:.1f} a Sua média é: {:.1f} \nVocê está {}REPROVADO!{}'.format(nota01, nota02, media, '\033[31m', '\033[m'))
     print()
 elif media < 6.9:
-    print('Com {:.1f} e {:.1f} a Sua média é: {:.1f} \nVocê está de {}RECUPERAÇÃO!{}'.format(nota01, nota02, media, '\033[34m', '\033[m'))
+    print('Com {:.1f} e {:.1f} a Sua média é: {:.1f} \nVocê está em {}RECUPERAÇÃO!{}'.format(nota01, nota02, media, '\033[34m', '\033[m'))
     print()
 else:
     print('Com {:.1f} e {:.1f} a Sua média é: {:.1f} \nVocê está {}APROVADO!{}'.format(nota01, nota02, media, '\033[32m', '\033[m'))
