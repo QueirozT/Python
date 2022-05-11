368525e8ea041d269a3e760407d632f3f62b82d9
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-11 19:15:34 -0300

    Update ex039 - Alistamento Militar.py

--- CursoEmVideo/Exercicios/ex039 - Alistamento Militar.py
+++ CursoEmVideo/Exercicios/ex039 - Alistamento Militar.py
@@ -10,11 +10,14 @@ atual = datetime.now().year
 idade = atual - ano
 
 if idade < 18:
+    print('Você nasceu em {} e tem {} anos.'.format(ano, idade))
     print('faltam {} anos para você poder se alistar!'.format(18 - idade))
     print()
 elif idade == 18:
+    print('Você nasceu em {} e tem {} anos.'.format(ano, idade))
     print('Você deve se alistar IMEDIATAMENTE!')
     print()
 else:
+    print('Você nasceu em {} e tem {} anos.'.format(ano, idade))
     print('Você já deveria ter se alistado há {} anos!'.format(idade - 18))
     print()
