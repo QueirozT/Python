b484076dd074a176aaf0dcc2e71856bbd7b14535
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-11 19:50:55 -0300

    Update ex040 - Calculando Média.py

--- CursoEmVideo/Exercicios/ex040 - Calculando Média.py
+++ CursoEmVideo/Exercicios/ex040 - Calculando Média.py
@@ -8,11 +8,11 @@ print()
 media = (nota01 + nota02) / 2
 
 if media < 5:
-    print('Sua média é: {:.1f} \nVocê está {}REPROVADO!{}'.format(media, '\033[31m', '\033[m'))
+    print('Com {:.1f} e {:.1f} a Sua média é: {:.1f} \nVocê está {}REPROVADO!{}'.format(nota01, nota02, media, '\033[31m', '\033[m'))
     print()
 elif media < 6.9:
-    print('Sua média é: {:.1f} \nVocê está de {}RECUPERAÇÃO!{}'.format(media, '\033[34m', '\033[m'))
+    print('Com {:.1f} e {:.1f} a Sua média é: {:.1f} \nVocê está de {}RECUPERAÇÃO!{}'.format(nota01, nota02, media, '\033[34m', '\033[m'))
     print()
 else:
-    print('Sua média é: {:.1f} \nVocê está {}APROVADO!{}'.format(media, '\033[32m', '\033[m'))
+    print('Com {:.1f} e {:.1f} a Sua média é: {:.1f} \nVocê está {}APROVADO!{}'.format(nota01, nota02, media, '\033[32m', '\033[m'))
     print()
