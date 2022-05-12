6820c533f7d139c6628aa8e5ba14f38f7f63287e
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 14:33:48 -0300

    Update ex053 - Detector de Palindromo.py

--- CursoEmVideo/Exercicios/ex053 - Detector de Palindromo.py
+++ CursoEmVideo/Exercicios/ex053 - Detector de Palindromo.py
@@ -1,14 +1,16 @@
 print('===== EXERCÍCIO #53 =====')
 print()
 
-frase = str(input('Digite uma frase: ')).strip().upper()
+frase = str(input('Digite uma frase: ')).strip().upper() # Remove os espaços em branco no início e no fim e transforma a frase em MAIUSCULA.
 
-frase = ''.join(frase.split())
+frase = ''.join(frase.split()) # Remove os espaços em branco entre as palavras.
 
-teste = frase[::-1]
+teste = frase[::-1] # Inverte a frase.
 
-if frase == teste:
+if frase == teste: # Verifica se a frase é um palíndromo comparando ela mesma ao contrário.
+    print('{}'.format(frase))
     print('É um palíndromo!')
 else:
+    print('{}'.format(frase))
     print('Não é um palíndromo!')
 print()
