9b19811539c04b43b05295100ae668b890bc2418
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 19:11:46 -0300

    Update ex051 - Progressão Aritmética.py

--- CursoEmVideo/Exercicios/ex051 - Progressão Aritmética.py
+++ CursoEmVideo/Exercicios/ex051 - Progressão Aritmética.py
@@ -1,12 +1,29 @@
 print('===== EXERCÍCIO #51 =====')
 print()
 
-valor = int(input('Digite um número: '))
-razao = int(input('Digite a razão: '))
+print('=-' * 30)
+print('{:^60}'.format('10 TERMOS DE UMA PA - PROGRESSÃO ARITMÉTICA'))
+print('=-' * 30)
+
+termo = int(input('Primeiro Termo? '))
+razao = int(input('Razão? '))
 print()
 
-print('Os 10 primeiros valores de uma PA de razão {} começando de {} são:'.format(razao, valor))
-for i in range(0, 10):
-    print('{:>3}° = {}'.format(i+1, valor))
-    valor += razao
+for c in range(0, 10):
+    print('{}'.format(termo), end=' → ')
+    termo += razao
+print('ACABOU!')
 print()
+
+
+# VERSÃO ALTERNATIVA DO PROGRAMA.
+
+# valor = int(input('Digite um número: '))
+# razao = int(input('Digite a razão: '))
+# print()
+
+# print('Os 10 primeiros valores de uma PA de razão {} começando de {} são:'.format(razao, valor))
+# for i in range(0, 10):
+#     print('{:>3}° = {}'.format(i+1, valor))
+#     valor += razao
+# print()
