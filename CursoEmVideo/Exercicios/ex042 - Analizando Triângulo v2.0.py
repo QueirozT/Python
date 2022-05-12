19253739b90ae2cfd2b4aba0bff0b4df42a4b5c2
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-11 21:46:15 -0300

    Update ex042 - Analizando Triângulo v2.0.py
    Adicionado comparador múltiplo e breve descrição com diferença entre comparador múltiplo de igualdade "==" e diferença "!="

--- CursoEmVideo/Exercicios/ex042 - Analizando Triângulo v2.0.py
+++ CursoEmVideo/Exercicios/ex042 - Analizando Triângulo v2.0.py
@@ -14,7 +14,7 @@ print()
 if (v01 + v02) > v03 and (v01 + v03) > v02 and (v02 + v03) > v01:
     print('Pode-se formar um triângulo com os valores fornecidos.')
     
-    if v01 == v02 and v01 == v03:
+    if v01 == v02 == v03: # O python permite comparações múltiplas entre variáveis.
         print('E o tipo de triângulo é: EQUILÁTERO')
         print()
     elif v01 == v02 or v01 == v03 or v02 == v03:
@@ -26,3 +26,9 @@ if (v01 + v02) > v03 and (v01 + v03) > v02 and (v02 + v03) > v01:
 else:
     print('Não é possível formar um triângulo com os valores fornecidos.')
     print()
+
+
+# elif v01 != v02 != v03 != v01:
+    # print('O tipo de triângulo é ESCALENO') 
+
+# Diferente de comparações multiplas "iguais", quando utilizo "!=", o python compara individualmente o valor com o posterior. então no caso de 3 ou mais variáveis, temos que informar o valor de todas as variáveis para que possam ser comparadas. no exemplo a cima foi comparado 1 e 2, 2 com 3 e 3 com 1 para fechar o ciclo.
