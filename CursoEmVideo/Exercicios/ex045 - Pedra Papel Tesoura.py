a0b6051bd24bcf9d14ea0d2872856bd60b0aa89a
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 10:24:40 -0300

    Update ex045 - Pedra Papel Tesoura.py
    Alterando a exibição do game e tempo de reação para maior imersão.

--- CursoEmVideo/Exercicios/ex045 - Pedra Papel Tesoura.py
+++ CursoEmVideo/Exercicios/ex045 - Pedra Papel Tesoura.py
@@ -1,11 +1,12 @@
 from random import randint
+from time import sleep
 
 print('===== EXERCÍCIO #45 =====')
 print()
 
-print('-=-' * 20)
-print('{:^60}'.format('1 - PEDRA | 2 - PAPEL | 3 - TESOURA'))
-print('-=-' * 20)
+print('-=' * 20)
+print('{:^40}'.format('1 - PEDRA | 2 - PAPEL | 3 - TESOURA'))
+print('-=' * 20)
 print()
 
 cpu = randint(1, 3)
@@ -24,48 +25,49 @@ else:
     elif 'TESOURA' in player:
         player = 3
 
+print('JO')
+sleep(2)
+
+print('KEN')
+sleep(2)
+
+print('PO!!!')
+
+print('-=' * 20)
 if player == 1 and cpu == 1:
-    print('EMPATE!')
-    print('AMBOS JOGARAM - PEDRA')
-    print()
+    print('{:^40}'.format('EMPATE!'))
+    print('{:^40}'.format('AMBOS JOGARAM - PEDRA'))
 elif player == 1 and cpu == 2:
-    print('VOCÊ PERDEU!')
-    print('VOCÊ JOGOU - PEDRA')
-    print('CPU JOGOU - PAPEL')
-    print()
+    print('{:^40}'.format('VOCÊ PERDEU!'))
+    print('{:^40}'.format('VOCÊ JOGOU - PEDRA'))
+    print('{:^40}'.format('CPU JOGOU - PAPEL'))
 elif player == 1 and cpu == 3:
-    print('VOCÊ GANHOU!')
-    print('VOCÊ JOGOU - PEDRA')
-    print('CPU JOGOU - TESOURA')
-    print()
+    print('{:^40}'.format('VOCÊ GANHOU!'))
+    print('{:^40}'.format('VOCÊ JOGOU - PEDRA'))
+    print('{:^40}'.format('CPU JOGOU - TESOURA'))
 elif player == 2 and cpu == 1:
-    print('VOCÊ GANHOU!')
-    print('VOCÊ JOGOU - PAPEL')
-    print('CPU JOGOU - PEDRA')
-    print()
+    print('{:^40}'.format('VOCÊ GANHOU!'))
+    print('{:^40}'.format('VOCÊ JOGOU - PAPEL'))
+    print('{:^40}'.format('CPU JOGOU - PEDRA'))
 elif player == 2 and cpu == 2:
-    print('EMPATE!')
-    print('AMBOS JOGARAM - PAPEL')
-    print()
+    print('{:^40}'.format('EMPATE!'))
+    print('{:^40}'.format('AMBOS JOGARAM - PAPEL'))
 elif player == 2 and cpu == 3:
-    print('VOCÊ PERDEU!')
-    print('VOCÊ JOGOU - PAPEL')
-    print('CPU JOGOU - TESOURA')
-    print()
+    print('{:^40}'.format('VOCÊ PERDEU!'))
+    print('{:^40}'.format('VOCÊ JOGOU - PAPEL'))
+    print('{:^40}'.format('CPU JOGOU - TESOURA'))
 elif player == 3 and cpu == 1:
-    print('VOCÊ PERDEU!')
-    print('VOCÊ JOGOU - TESOURA')
-    print('CPU JOGOU - PEDRA')
-    print()
+    print('{:^40}'.format('VOCÊ PERDEU!'))
+    print('{:^40}'.format('VOCÊ JOGOU - TESOURA'))
+    print('{:^40}'.format('CPU JOGOU - PEDRA'))
 elif player == 3 and cpu == 2:
-    print('VOCÊ GANHOU!')
-    print('VOCÊ JOGOU - TESOURA')
+    print('{:^40}'.format('VOCÊ GANHOU!'))
+    print('{:^40}'.format('VOCÊ JOGOU - TESOURA'))
     print('CPU JOGOU - PAPEL')
-    print()
 elif player == 3 and cpu == 3:
-    print('EMPATE!')
-    print('AMBOS JOGARAM - TESOURA')
-    print()
+    print('{:^40}'.format('EMPATE!'))
+    print('{:^40}'.format('AMBOS JOGARAM - TESOURA'))
 else:
-    print('Opção inválida!')
-    print()
+    print('{:^40}'.format('Opção inválida!'))
+print('-=' * 20)
+print()
