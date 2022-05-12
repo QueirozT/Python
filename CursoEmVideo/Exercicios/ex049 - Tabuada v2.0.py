1ef7f868bef092af9bad85d2fa484fe94c707b91
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 18:40:36 -0300

    Update ex049 - Tabuada v2.0.py

--- CursoEmVideo/Exercicios/ex049 - Tabuada v2.0.py
+++ CursoEmVideo/Exercicios/ex049 - Tabuada v2.0.py
@@ -37,3 +37,9 @@ else:
     print('Opção inválida!')
     print()
     
+
+# VERSÃO SIMPLIFICADA DO PROGRAMA:
+
+# num = int(input('Digite um número para ver a tabuada! '))
+# for c in range(1, 11):
+#     print('{} x {:2} = {}'.format(num, c, num * c))
