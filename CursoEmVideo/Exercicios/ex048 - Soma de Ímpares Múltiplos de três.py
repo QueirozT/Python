6e34bf994f08fd3498034cb9bf75e0946712c81a
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 18:32:21 -0300

    Update ex048 - Soma de Ímpares Múltiplos de três.py

--- CursoEmVideo/Exercicios/ex048 - Soma de Ímpares Múltiplos de três.py
+++ CursoEmVideo/Exercicios/ex048 - Soma de Ímpares Múltiplos de três.py
@@ -2,10 +2,12 @@ print('===== EXERCÍCIO #48 =====')
 print()
 
 contagem = 0
+soma = 0
 
-for i in range(1, 501):
+for i in range(1, 501, 2):
     if i % 3 == 0:
         contagem += i
+        soma += 1
 
-print('A soma dos valores impares múltiplos de 3 entre 1 e 500 é: {}'.format(contagem))
+print('A soma de todos os {} valores impares múltiplos de 3 entre 1 e 500 é: {}'.format(soma, contagem))
 print()
