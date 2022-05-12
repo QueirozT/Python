b8890b685bf8066b8a2a68e4ed03da9dadaf34ec
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 14:51:15 -0300

    Create ex054 - Grupo de Maior Idade.py

---
+++ CursoEmVideo/Exercicios/ex054 - Grupo de Maior Idade.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+print('===== EXERCÍCIO #54 =====')
+print()
+
+maior = 0
+menor = 0
+
+atual = datetime.now().year
+
+for i in range(0, 7):
+    ano = int(input('Digite o ano de nascimento: '))
+
+    idade = atual - ano
+
+    if idade >= 18:
+        maior += 1
+    elif idade < 18:
+        menor += 1
+print()
+
+if maior > 0 and menor > 0:
+    print('Dos 7 que você digitou, {} são maiores de idade e {} são menores de idade.'.format(maior, menor))
+elif maior > 0 and menor == 0:
+    print('Dos 7 que você digitou, {} são maiores de idade.'.format(maior))
+elif maior == 0 and menor > 0:
+    print('Dos 7 que você digitou, {} são menores de idade.'.format(menor))
+print()
