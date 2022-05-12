0bed01a4d780f8d6b4c35c4c4428bd6f72a6fe53
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 14:39:36 -0300

    Update ex053 - Detector de Palindromo.py

--- CursoEmVideo/Exercicios/ex053 - Detector de Palindromo.py
+++ CursoEmVideo/Exercicios/ex053 - Detector de Palindromo.py
@@ -9,9 +9,9 @@ semEspaco = ''.join(frase.split()) # Remove os espaços em branco entre as palav
 aoContrario = semEspaco[::-1] # Inverte a frase.
 
 if semEspaco == aoContrario: # Verifica se a frase é um palíndromo comparando ela mesma ao contrário.
-    print('{}'.format(frase))
+    print('"{}"'.format(frase))
     print('É UM PALÍNDROMO!')
 else:
-    print('{}'.format(frase))
-    print('Não É UM PALÍNDROMO!')
+    print('"{}"'.format(frase))
+    print('NÃO É UM PALÍNDROMO!')
 print()
