eeddd4b26cf4c5ef88dde3069b3e70b6e88f9d6c
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 19:59:23 -0300

    Update ex053 - Detector de Palindromo.py

--- CursoEmVideo/Exercicios/ex053 - Detector de Palindromo.py
+++ CursoEmVideo/Exercicios/ex053 - Detector de Palindromo.py
@@ -4,14 +4,35 @@ print()
 frase = str(input('Digite uma frase: ')).strip().upper() # Remove os espaços em branco no início e no fim e transforma a frase em MAIUSCULA.
 print()
 
-semEspaco = ''.join(frase.split()) # Remove os espaços em branco entre as palavras.
+frase = ''.join(frase.split()) # Remove os espaços em branco entre as palavras.
 
-aoContrario = semEspaco[::-1] # Inverte a frase.
+aoContrario = frase[::-1] # Inverte a frase.
 
-print('O  inverso de {} é {}.'.format(semEspaco, aoContrario))
+print('O  inverso de {} é {}.'.format(frase, aoContrario))
 
-if semEspaco == aoContrario: # Verifica se a frase é um palíndromo comparando ela mesma ao contrário.
+if frase == aoContrario: # Verifica se a frase é um palíndromo comparando ela mesma ao contrário.
     print('TEMOS UM PALÍNDROMO!')
 else:
     print('A frase digitada NÃO É UM PALÍNDROMO!')
 print()
+
+
+# VERSÃO ALTERNATIVA USANDO O FOR:
+
+# frase = str(input('Escreva uma frase: ')).strip().upper()
+# print()
+
+# frase = ''.join(frase.split())
+
+# inverso = ''
+
+# for letra in range(len(frase) - 1, -1, -1):
+#     inverso += frase[letra]
+
+# print('O inverso de {} é {}.'.format(frase, inverso))
+
+# if frase == inverso:
+#     print('Temos um palíndromo!')
+# else:
+#     print('A frase digitada não é um palíndromo!')
+# print()
