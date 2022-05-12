25b93e86152968e606b58fef8efad259b533731f
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 18:56:28 -0300

    Update ex050 - Soma dos Pares.py

--- CursoEmVideo/Exercicios/ex050 - Soma dos Pares.py
+++ CursoEmVideo/Exercicios/ex050 - Soma dos Pares.py
@@ -2,12 +2,14 @@ print('===== EXERCÍCIO #50 =====')
 print()
 
 soma = 0
+contador = 0
 
-for i in range(0, 6):
-    numero = int(input('Digite um número: '))
+for i in range(1, 7):
+    numero = int(input('Digite o {}° valor: '.format(i)))
     if numero % 2 == 0:
         soma += numero
+        contador += 1
 print()
 
-print('A soma dos números pares é: {}'.format(soma))
+print('A soma dos {} números pares é {}'.format(contador, soma))
 print()
