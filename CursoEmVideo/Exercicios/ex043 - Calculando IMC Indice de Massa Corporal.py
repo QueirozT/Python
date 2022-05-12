8c2b13559f775d84c6206dc477e2c6b8a8a15947
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-11 22:29:45 -0300

    Update ex043 - Calculando IMC Indice de Massa Corporal.py
    Adicionando tratamento de erros (simples) para identificar se o que foi digitado era um número.

--- CursoEmVideo/Exercicios/ex043 - Calculando IMC Indice de Massa Corporal.py
+++ CursoEmVideo/Exercicios/ex043 - Calculando IMC Indice de Massa Corporal.py
@@ -5,37 +5,51 @@ altura = input('Qual a sua altura? ')
 peso = input('Qual o seu peso? ')
 print()
 
-# Tratamento de erros (simples) para a altura
-if str(altura).isdecimal():
-    altura = float(altura[:1] + '.' + altura[1:])
+# Tratamento de erros (simples) para identificar se algum caractere não numérico foi inserido.
+if str(altura).isalpha() or str(peso).isalpha():
+    numero = False
 else:
-    altura = float(altura)
+    numero = True
 
-# Tratamento de erros (simples) para o peso
-if str(peso).isdecimal():
-    peso = float(peso[:2] + '.' + peso[2:])
+# Tratamento de erros (simples) para a conversão da altura.
+if numero:
+    if str(altura).isdecimal():
+        altura = float(altura[:1] + '.' + altura[1:])
+    else:
+        altura = float(altura)
+
+# Tratamento de erros (simples) para a conversão do peso.
+if numero:
+    if str(peso).isdecimal():
+        peso = float(peso[:2] + '.' + peso[2:])
+    else:
+        peso = float(peso)
+
+if numero:
+    imc = peso / (altura * altura)
+
+
+if numero:
+    if imc < 18.5:
+        print('Seu IMC é de: {:.1f}'.format(imc))
+        print('Você está abaixo do peso.')
+        print()
+    elif imc < 25:
+        print('Seu IMC é de: {:.1f}'.format(imc))
+        print('Você está no peso ideal.')
+        print()
+    elif imc < 30:
+        print('Seu IMC é de: {:.1f}'.format(imc))
+        print('Você está com sobrepeso.')
+        print()
+    elif imc < 40:
+        print('Seu IMC é de: {:.1f}'.format(imc))
+        print('Você está com obesidade.')
+        print()
+    else:
+        print('Seu IMC é de: {:.1f}'.format(imc))
+        print('Você está com obesidade mórbida.')
+        print()
 else:
-    peso = float(peso)
-
-imc = peso / (altura * altura)
-
-if imc < 18.5:
-    print('Seu IMC é de: {:.1f}'.format(imc))
-    print('Você está abaixo do peso.')
-    print()
-elif imc < 25:
-    print('Seu IMC é de: {:.1f}'.format(imc))
-    print('Você está no peso ideal.')
-    print()
-elif imc < 30:
-    print('Seu IMC é de: {:.1f}'.format(imc))
-    print('Você está com sobrepeso.')
-    print()
-elif imc < 40:
-    print('Seu IMC é de: {:.1f}'.format(imc))
-    print('Você está com obesidade.')
-    print()
-else:
-    print('Seu IMC é de: {:.1f}'.format(imc))
-    print('Você está com obesidade mórbida.')
+    print('Não é possível calcular o IMC com esses dados.')
     print()
