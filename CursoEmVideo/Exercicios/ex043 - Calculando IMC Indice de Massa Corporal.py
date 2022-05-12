3c04bf843db7db6404f9d9347cf0d08d80204000
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-11 22:02:49 -0300

    Update ex043 - Calculando IMC Indice de Massa Corporal.py
    Adicionando tratamento de erros (simples) para a entrada de dados numericos.

--- CursoEmVideo/Exercicios/ex043 - Calculando IMC Indice de Massa Corporal.py
+++ CursoEmVideo/Exercicios/ex043 - Calculando IMC Indice de Massa Corporal.py
@@ -1,29 +1,41 @@
 print('===== EXERCÍCIO #43 =====')
 print()
 
-altura = float(input('Qual a sua altura? '))
-peso = float(input('Qual o seu peso? '))
+altura = input('Qual a sua altura? ')
+peso = input('Qual o seu peso? ')
 print()
+
+# Tratamento de erros (simples) para a altura
+if str(altura).isdecimal():
+    altura = float(altura[:1] + '.' + altura[1:])
+else:
+    altura = float(altura)
+
+# Tratamento de erros (simples) para o peso
+if str(peso).isdecimal():
+    peso = float(peso[:2] + '.' + peso[2:])
+else:
+    peso = float(peso)
 
 imc = peso / (altura * altura)
 
 if imc < 18.5:
-    print('Seu IMC é de: {:.2f}'.format(imc))
+    print('Seu IMC é de: {:.1f}'.format(imc))
     print('Você está abaixo do peso.')
     print()
 elif imc < 25:
-    print('Seu IMC é de: {:.2f}'.format(imc))
+    print('Seu IMC é de: {:.1f}'.format(imc))
     print('Você está no peso ideal.')
     print()
 elif imc < 30:
-    print('Seu IMC é de: {:.2f}'.format(imc))
+    print('Seu IMC é de: {:.1f}'.format(imc))
     print('Você está com sobrepeso.')
     print()
 elif imc < 40:
-    print('Seu IMC é de: {:.2f}'.format(imc))
+    print('Seu IMC é de: {:.1f}'.format(imc))
     print('Você está com obesidade.')
     print()
 else:
-    print('Seu IMC é de: {:.2f}'.format(imc))
+    print('Seu IMC é de: {:.1f}'.format(imc))
     print('Você está com obesidade mórbida.')
     print()
