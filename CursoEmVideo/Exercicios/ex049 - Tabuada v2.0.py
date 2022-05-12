3e3c426f00d793214487ee3c94af4e310bc490ce
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 18:45:50 -0300

    Update ex049 - Tabuada v2.0.py

--- CursoEmVideo/Exercicios/ex049 - Tabuada v2.0.py
+++ CursoEmVideo/Exercicios/ex049 - Tabuada v2.0.py
@@ -16,22 +16,22 @@ print()
 if opcao == 1:
     print('{:^22}'.format('TABUADA DE ADIÇÃO:'))
     for i in range(1, 11):
-        print('{:^22}'.format('{}  +  {:<3}  =  {}'.format(valor, i, valor + i)))
+        print('{:^22}'.format('{}  +  {:<3} =  {}'.format(valor, i, valor + i)))
     print()
 elif opcao == 2:
     print('{:^22}'.format('TABUADA DE SUBTRAÇÃO:'))
     for i in range(1, 11):
-        print('{:^22}'.format('{}  -  {:<3}  =  {}'.format(valor, i, valor - i)))
+        print('{:^22}'.format('{}  -  {:<3} =  {}'.format(valor, i, valor - i)))
     print()
 elif opcao == 3:
     print('{:^22}'.format('TABUADA DE MULTIPLICAÇÃO:'))
     for i in range(1, 11):
-        print('{:^22}'.format('{}  x  {:<3}  =  {}'.format(valor, i, valor * i)))
+        print('{:^22}'.format('{}  x  {:<3} =  {}'.format(valor, i, valor * i)))
     print()
 elif opcao == 4:
     print('{:^22}'.format('TABUADA DE DIVISÃO:'))
     for i in range(1, 11):
-        print('{:^22}'.format('{}  /  {:<3}  =  {:<10.1f}'.format(valor, i, valor / i)))
+        print('{:^22}'.format('{}  /  {:<3} =  {:<5.1f}'.format(valor, i, valor / i)))
     print()
 else:
     print('Opção inválida!')
