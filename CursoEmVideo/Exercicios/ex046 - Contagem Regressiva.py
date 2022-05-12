d8611a4c2e114b11de2c65acf155672a1f046ba9
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 11:50:05 -0300

    Update ex046 - Contagem Regressiva.py

--- CursoEmVideo/Exercicios/ex046 - Contagem Regressiva.py
+++ CursoEmVideo/Exercicios/ex046 - Contagem Regressiva.py
@@ -3,11 +3,13 @@ from time import sleep
 print('===== EXERCÍCIO #46 =====')
 print()
 
-print('INICIANDO CONTAGEM REGRESSIVA PARA O LANÇAMENTO...')
-sleep(2)
+print('INICIANDO CONTAGEM REGRESSIVA...')
+sleep(1)
+print()
 
 for i in range(10, -1, -1):
     print(i)
     sleep(1)
 
-print('*EXPLOSÕES*')
+print('*LANÇANDO OS FOGOS!*')
+print()
