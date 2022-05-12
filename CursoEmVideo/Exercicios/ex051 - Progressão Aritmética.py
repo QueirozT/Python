6598d2b3f6f895d9fcdd549af798f01fdc038b4a
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-12 19:21:34 -0300

    Update ex051 - Progressão Aritmética.py

--- CursoEmVideo/Exercicios/ex051 - Progressão Aritmética.py
+++ CursoEmVideo/Exercicios/ex051 - Progressão Aritmética.py
@@ -9,9 +9,10 @@ termo = int(input('Primeiro Termo? '))
 razao = int(input('Razão? '))
 print()
 
-for c in range(0, 10):
-    print('{}'.format(termo), end=' → ')
-    termo += razao
+decimo = termo + (10 - 1) * razao # Fórmula matemática para identificar o 10° termos de uma PA.
+
+for c in range(termo, decimo + razao, razao):
+    print('{}'.format(c), end=' → ')
 print('ACABOU!')
 print()
 
