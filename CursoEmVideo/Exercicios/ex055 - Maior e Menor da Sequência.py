960b9b89ceae40907a5800577227d8926582e953
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-13 22:04:50 -0300

    Update ex055 - Maior e Menor da Sequência.py

--- CursoEmVideo/Exercicios/ex055 - Maior e Menor da Sequência.py
+++ CursoEmVideo/Exercicios/ex055 - Maior e Menor da Sequência.py
@@ -10,3 +10,24 @@ print()
 print('O maior valor é {}'.format(max(peso))) # max() retorna o maior valor de um iterável.
 print('O menor valor é {}'.format(min(peso))) # min() retorna o menor valor de um iterável.
 print()
+
+
+# VERSÃO ALTERNATIVA DO PROGRAMA:
+
+# maior = 0
+# menor = 0
+
+# for i in range(5):
+#     peso = int(input("Qual o peso da {}ª pessoa? ".format(i+1)))
+#     if i == 0:
+#         maior = peso
+#         menor = peso
+#     if peso > maior:
+#         maior = peso
+#     if peso < menor:
+#         menor = peso
+# print()
+
+# print('O maior valor registrador foi {}'.format(maior))
+# print('E o menor valor registrador foi {}'.format(menor))
+# print()
