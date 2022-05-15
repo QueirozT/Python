d7e8df321a0d5fe71797b5be25cfaf036a3096e5
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-15 00:21:36 -0300

    Create ex063 - Super Progressão Aritmética v3.0.py

---
+++ CursoEmVideo/Exercicios/ex063 - Super Progressão Aritmética v3.0.py
@@ -0,0 +1,38 @@
+print('===== EXERCÍCIO #62 =====')
+print()
+
+print('=-' * 20)
+print('{:^40}'.format('PROGRESSÃO ARITMÉTICA'))
+print('=-' * 20)
+print()
+
+valor = int(input('Qual o número? '))
+r = int(input('Qual a razão? '))
+print()
+
+c = 10
+
+while c != 0:
+    print('{}'.format(valor), end=" -> ")
+    valor += r
+    c -= 1
+print('FIM!')
+print()
+
+# Segunda parte do programa, responsável por mostrar mais resultados da progressão aritmética.
+mais = input('Quer mostrar mais termos? [S/N]: ').upper().strip()
+print()
+
+while mais == 'S':
+    c = int(input('Quantos termos você quer mostrar a mais? '))
+    print()
+
+    while c != 0:
+        print('{}'.format(valor), end=" -> ")
+        valor += r
+        c -= 1
+    print('FIM!')
+    print()
+
+    mais = input('Quer mostrar mais termos? [S/N]: ').upper().strip()
+    print()
