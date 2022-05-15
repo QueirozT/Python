130d6a5f457e799ad720207f3528377d665eb55e
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-15 20:46:45 -0300

    Update ex057 - Validação de Dados.py

--- CursoEmVideo/Exercicios/ex057 - Validação de Dados.py
+++ CursoEmVideo/Exercicios/ex057 - Validação de Dados.py
@@ -9,3 +9,16 @@ while sexo != 'F' and sexo != 'M':
         print('"{}" não é um valor válido. Tente novamente!'.format(sexo))
 print('Sexo "{}" registrado com sucesso!'.format(sexo))
 print()
+
+
+# PROGRAMA ALTERNATIVO:
+
+# sexo = str(input('Informe seu sexo [M/F]: ')).upper().strip()[0] # Convertendo e fatiando a string.
+
+# while sexo not in 'MF':
+#     print('"{}" Não é um valor inválido!'.format(sexo))
+#     print()
+#     sexo = str(input('Informe seu sexo [M/F]: ')).upper().strip()[0]
+#     print()
+# print('Sexo "{}" registrado com sucesso!'.format(sexo))
+# print()
