60e94a73e866aef550eb7503cbbd0967fa2f8297
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-16 15:27:44 -0300

    Update ex060 - Cálculo do Fatorial.py
    Adicionando outra forma de criando o programa

--- CursoEmVideo/Exercicios/ex060 - Cálculo do Fatorial.py
+++ CursoEmVideo/Exercicios/ex060 - Cálculo do Fatorial.py
@@ -16,6 +16,7 @@ print('O fatorial de {} é {}'.format(valor, fatorial))
 print()
 
 
+
 # PROGRAMA ALTERNATIVO:
 
 # from math import factorial
@@ -25,4 +26,25 @@ print()
 # f = factorial(n)
 
 # print('O fatorial de {} é {}'.format(n, f))
+# print()
+
+
+
+# PROGRAMA ALTERNATIVO 2:
+
+# n = int(input('Digite um número para calcular seu fatorial: '))
+# print()
+
+# c = n
+# f = 1
+
+# print('Calculando {}! = '.format(n), end='')
+
+# while c > 0:
+#     print('{}'.format(c), end='')
+#     print(' x ' if c > 1 else ' = ', end='')
+#     f *= c
+#     c -= 1
+
+# print('{}'.format(f))
 # print()
