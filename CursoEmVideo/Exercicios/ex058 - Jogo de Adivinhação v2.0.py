91c9a3c381010566e047842b1185423df97f347d
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-15 21:10:29 -0300

    Update ex058 - Jogo de Adivinhação v2.0.py
    Alterando o programa para ficar mais fácil de ler e mais completo.

--- CursoEmVideo/Exercicios/ex058 - Jogo de Adivinhação v2.0.py
+++ CursoEmVideo/Exercicios/ex058 - Jogo de Adivinhação v2.0.py
@@ -1,3 +1,4 @@
+from ast import Break
 from random import randint
 
 print('===== EXERCÍCIO #58 =====')
@@ -10,18 +11,29 @@ print()
 
 cpu = randint(0, 10)
 
-print('Estou pensando em um número entre 0 e 10. Tente adivinhar...')
+print('Estou pensando em um número entre 0 e 10...')
 print()
 
 tentativas = 0
 
-jogador = ''
+jogador = int(input('Qual é o seu palpite? '))
 
 while jogador != cpu:
+    if jogador > 10 or jogador < 0:
+        print('"{}" não vale! tente de novo.'.format(jogador))
+        print()
+    elif jogador < cpu:
+        print('Você errou! é um pouco mais...')
+        print()
+    elif jogador > cpu:
+        print('Você errou! é um pouco menos...')
+        print()
     jogador = int(input('Qual é o seu palpite? '))
     tentativas += 1
-    if jogador != cpu:
-        print('Você errou! Tente novamente!')
-        print()
-print('Parabéns! Você acertou, mas precisou de {} tentativas.'.format(tentativas))
-print()
+
+if tentativas == 0:
+    print('Parabéns! Você acertou de primeira!')
+    print()
+else:
+    print('Parabéns! Você acertou, mas precisou de {} tentativas.'.format(tentativas))
+    print()
