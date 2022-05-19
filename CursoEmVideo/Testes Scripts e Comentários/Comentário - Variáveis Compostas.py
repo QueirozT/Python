f5267b36a7d72d1598ea1907288b3f43a86ad27f
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-19 09:54:31 -0300

    Update Comentário - Variáveis Compostas.py

--- CursoEmVideo/Testes Scripts e Comentários/Comentário - Variáveis Compostas.py
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Variáveis Compostas.py
@@ -1,1 +1,7 @@
-# TUPLAS - 
+# TUPLAS - As tuplas são variáveis compostas que armazenam mais de um valor dentro de (). Após serem criadas, não podem ser alteradas.
+
+# EXEMPLO 01: Criando uma tupla (O uso de () é opcional)
+print('EXEMPLO 01: Criando uma tupla')
+tupla = (1, 2, 3, 'Valor 01', 'Valor 02', 'Valor 03')
+print(tupla)
+print()
