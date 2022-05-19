2244878e2b1a6364da50811b72f521236c1bbf5f
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-19 20:13:13 -0300

    Update ex074 - Maior e Menor Valores em Tupla.py

--- CursoEmVideo/Exercicios/ex074 - Maior e Menor Valores em Tupla.py
+++ CursoEmVideo/Exercicios/ex074 - Maior e Menor Valores em Tupla.py
@@ -5,11 +5,11 @@ print()
 
 tupla = ()
 
-for i in range(4):
+for i in range(5):
     valor = (randint(0, 10),) # O '(,)' é necessário para que o valor seja uma tupla.
     tupla += valor # Somente uma tupla pode ser concatenada com outra tupla.
 
-print(f'Os valores sorteados foram: {tupla[0:]}')
+print(f'Os valores sorteados foram: {tupla}')
 print()
 
 print(f'O maior valor sorteado foi {sorted(tupla)[-1]}')
