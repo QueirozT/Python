017e720a94769a5a67aa6698eb3a597a45b5f8ef
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-19 22:01:15 -0300

    Update ex075 - Análise de Dados em Tuplas.py

--- CursoEmVideo/Exercicios/ex075 - Análise de Dados em Tuplas.py
+++ CursoEmVideo/Exercicios/ex075 - Análise de Dados em Tuplas.py
@@ -10,13 +10,13 @@ print()
 print(f'Você digitou os valores: {tupla}')
 print()
 
-if tupla.count(9) > 0:
+if tupla.count(9) > 0: # Verifica se existe o número 9 dentro da tupla.
     print(f'O valor 9 aparece {tupla.count(9)} vez(es).')
 else:
     print('O valor 9 não foi digitado.')
 print()
 
-if tupla.count(3) > 0:
+if 3 in tupla: # Usando o in para procurar um valor dentro da tupla.
     print(f'O valor 3 foi encontrado na {tupla.index(3)+1}º posição.')
 else:
     print('O valor 3 não foi digitado.')
