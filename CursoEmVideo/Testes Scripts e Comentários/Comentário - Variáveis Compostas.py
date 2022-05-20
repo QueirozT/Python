1a688da7f8be5270a1dd2b46c74d114e4c2f5646
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-20 12:32:22 -0300

    Update Comentário - Variáveis Compostas.py

--- CursoEmVideo/Testes Scripts e Comentários/Comentário - Variáveis Compostas.py
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Variáveis Compostas.py
@@ -18,3 +18,37 @@ print()
 
 # Apesar de não permitir alterações, as tuplas podem ser somadas em uma nova variável ou convertidas em listas.
 
+
+
+# LISTAS - As listas são variáveis compostas que armazenam mais de um valor dentro de [] (Colchetes). Estas podem ser alteradas.
+
+# EXEMPLO 02: Criando uma lista
+print('EXEMPLO 02: Criando uma lista')
+lista = [1, 2, 3, 'Valor 01', 'Valor 02', 'Valor 03']
+print(lista)
+print()
+
+
+# EXEMPLO 03: Criando uma lista através do metodo list()
+print('EXEMPLO 03: Criando uma lista através do metodo list()')
+lista = list(range(1, 11))
+print(lista)
+print()
+
+
+# AS LISTAS TAMBÉM POSSUEM MÉTODOS, COMO:
+# .append(valor) - Adiciona um valor no final da lista.
+# .insert(indice, valor) - Insere um valor em um índice específico sem substituir valores existentes.
+
+# len(lista) - Retorna o tamanho da lista.
+# .index(valor) - Retorna o índice da primeira ocorrência de um valor. Pode procurar a partir de um índice específico. EX: .index('t', 2).
+# .count(valor) - Retorna a quantidade de ocorrências de um valor dentro da lista.
+
+# .sort() - Ordena a lista. Pode inverter a ordem dos elementos com o parâmetro .sort(reverse=True).
+# .reverse() - Inverte a ordem dos elementos da lista.
+
+# .copy() - Cria uma cópia da lista.
+# .remove(valor) - Remove o primeiro "valor" que encontrar.
+# .pop(indice) - Remove o valor que está em um índice específico. Se não especificado, o último valor da lista será removido.
+# del lista[indice] - Remove um valor específico a lista.
+# .clear() - Limpa a lista.
