dc9671920c55d6352bb3951040efcc97f747af93
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-21 14:59:49 -0300

    Update ex083 - Validando Expressões Matemáticas.py
    Adicionando versão alternativa utilizando vetores.

--- CursoEmVideo/Exercicios/ex083 - Validando Expressões Matemáticas.py
+++ CursoEmVideo/Exercicios/ex083 - Validando Expressões Matemáticas.py
@@ -6,8 +6,31 @@ valor = input('Digite uma expressão matemática: ')
 abertura = valor.count('(')
 fechamento = valor.count(')')
 
-if abertura == fechamento:
+if abertura == fechamento and valor.find('(') < valor.find(')'):
     print('Expressão válida!')
 else:
     print('Expressão inválida!')
 print()
+
+
+
+# VERSÃO ALTERNATIVA:
+
+# expr = input('Digite uma expressão matemática: ')
+# pilha = []
+
+# for i in expr:
+#     if i == '(':
+#         pilha.append('(')
+#     elif i == ')':
+#         if len(pilha) > 0:
+#             pilha.pop()
+#         else:
+#             pilha.append(')')
+#             break
+
+# if len(pilha) == 0:
+#     print('Expressão válida!')
+# else:
+#     print('Expressão inválida!')
+# print()
