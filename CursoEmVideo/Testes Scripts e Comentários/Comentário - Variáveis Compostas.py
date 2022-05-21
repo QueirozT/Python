daf3cd3528cf29ffc6bd5b15f5dcd6abf7c77829
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-21 15:23:39 -0300

    Update Comentário - Variáveis Compostas.py

--- CursoEmVideo/Testes Scripts e Comentários/Comentário - Variáveis Compostas.py
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Variáveis Compostas.py
@@ -59,3 +59,36 @@ print()
 # .clear() - Limpa a lista.
 
 # ======================================================================================================================
+
+
+# LISTAS COMPOSTAS - As listas compostas são listas que armazenam outras listas, e podem ser acessadas como listas normais.
+
+# As listas compostas também possúem índice, porém dentro de cada índice existe uma lista com o próprio índice. Exemplo:
+
+# lista_composta = [lista_01, lista_02, lista_03]
+# lista_01 = [1, 2, 3]
+# lista_02 = [4, 5, 6]
+# lista_03 = [7, 8, 9]
+
+# lista_composta = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# Para acessar os valores de uma lista composta, basta utilizar o índice da lista que está dentro da lista composta. 
+# EX:
+# lista_composta[0][0] = 1
+# lista_composta[0][1] = 2
+# lista_composta[0][2] = 3
+
+
+# EXEMPLO 04: Criando uma lista composta
+print('EXEMPLO 04: Criando uma lista composta')
+lista_composta = [['Pedro', 32], ['Maria', 25], ['João', 28]]
+print(lista_composta)
+print()
+
+# EXEMPLO 05: Acessando os valores de uma lista composta
+print('EXEMPLO 05: Acessando os nomes da lista composta do exercício anterior')
+print(lista_composta[0][0])
+print(lista_composta[1][0])
+print(lista_composta[2][0])
+print()
+
