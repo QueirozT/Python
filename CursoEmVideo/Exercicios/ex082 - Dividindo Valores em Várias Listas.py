33646eddc6cd60bfc28c3f8596c0633f1bab2dc5
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-21 14:08:00 -0300

    Create ex082 - Dividindo Valores em Várias Listas.py

---
+++ CursoEmVideo/Exercicios/ex082 - Dividindo Valores em Várias Listas.py
@@ -0,0 +1,31 @@
+print('===== EXERCÍCIO #082 =====')
+print()
+
+# Inicializando as variáveis
+lista = par = impar = list()
+
+while True:
+    valor = int(input('Digite um valor: '))
+    lista.append(valor) # Adicionando o valor na lista principal
+
+# Verificando se o valor é par ou impar e adicionando na lista correspondente.
+    if valor % 2 == 0:
+        par.append(valor)
+    else:
+        impar.append(valor)
+
+# Verificando se o usuário quer continuar
+    while True:
+        opcao = input('Deseja continuar? [S/N] ').upper().strip()[0]
+        if opcao in 'SN':
+            break
+        print('Resposta inválida!')
+    print()
+
+    if opcao == 'N':
+        break
+
+print(f'A lista completa é {lista}')
+print(f'A lista de pares é {par}')
+print(f'A lista de ímpares é {impar}')
+print()
