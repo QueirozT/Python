cab53befe30b12288069bbdf172a1a51f8ffe7ee
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-22 23:21:11 -0300

    Update ex090 - Dicionário em Python.py

--- CursoEmVideo/Exercicios/ex090 - Dicionário em Python.py
+++ CursoEmVideo/Exercicios/ex090 - Dicionário em Python.py
@@ -1,17 +1,22 @@
 print('===== EXERCÍO #090 =====')
 print()
 
-aluno = dict()
+aluno = dict() # Cria um dicionário vazio
 
-aluno['Nome'] = input('Nome: ')
-aluno['Média'] = float(input('Média: '))
+# Adiciona os valores ao dicionário
+aluno['Nome'] = str(input('Qual o nome do aluno? ')).strip().title()
+aluno['Média'] = float(input(f'Qual a média de {aluno["Nome"]}? '))
 print()
 
-if aluno['Média'] < 5:
-    aluno['Situação'] = 'Reprovado!'
-else:
+# Verificando se o aluno foi aprovado ou reprovado e armazenando no dicionário.
+if aluno['Média'] >= 7:
     aluno['Situação'] = 'Aprovado!'
+elif 5 <= aluno['Média'] < 7:
+    aluno['Situação'] = 'Recuperação!'
+else:
+    aluno['Situação'] = 'Reprovado!'
 
-for i, v in aluno.items():
-    print(f'{i} é {v}')
+# Varrendo o dicionário e exibindo os valores.
+for k, v in aluno.items(): # Varrendo os valores Key 'k' e Value 'v' do dicionário.
+    print(f'{k} é {v}') # Imprimindo as Chaves e os Valores.
 print()
