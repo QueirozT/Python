50196d442934967212eb453b52da771a989ae52e
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-24 11:55:44 -0300

    Update Comentário - Funções e Parâmetros.py
    Adicionando Descrições para facilitar a visualização dos exemplos.

--- CursoEmVideo/Testes Scripts e Comentários/Comentário - Funções e Parâmetros.py
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Funções e Parâmetros.py
@@ -19,9 +19,11 @@ def lin():
 
 # Para chamar ou executar uma rotina (FUNÇÃO), é só utilizar o nome da função.
 # EXEMPLO: Chamando a função lin() para mostrar uma linha.
+print('EX 01 - Chamando uma função simpels.')
 lin()
 
 #=======================================================================================================================
+
 
 
 # PARÂMETROS: Parâmetros são variáveis que são passadas para uma função.
@@ -46,7 +48,9 @@ def msg(text):
 # Como chamar uma função com parâmetros?
 # Para chamar uma função com parâmetros, é só colocar os valores dos parâmetros entre os parênteses() da função.
 # EXEMPLO: Chamando a função msg() com o texto dentro do parâmetro.
+print('\n EX 02 - Chamando uma função com parâmetros.')
 msg('Mensagem de Teste!')
+
 
 
 # É possível definir mais de um parâmetro.
@@ -60,7 +64,9 @@ def soma(a, b):
 # Como chamar uma função com mais de um parâmetro?
 # Para chamar uma função com mais de um parâmetro, é só colocar os valores dos parâmetros entre os parênteses() da função.
 # EXEMPLO: Chamando a função soma() com os valores dentro dos parâmetros.
+print('\n EX 03 - Chamando uma função com mais de um parâmetro.')
 soma(10, 20)
+
 
 
 # Retornando o resultado de uma série de operações.
@@ -82,7 +88,9 @@ def fat(num):
 
 # Como chamar uma função que retorna um valor?
 # EXEMPLO: Chamando a função fat() com o valor que será calculado dentro do parâmetro e exibindo o resultado.
+print('\n EX 04 - Chamando uma função que retorna o resultado de operações.')
 print('O fatorial de 4 é: ', fat(4))
+
 
 
 # Passando F-Strings e outras funções dentro de uma função com formatação de texto.
@@ -90,6 +98,44 @@ print('O fatorial de 4 é: ', fat(4))
 
 # Passando um valor com F-String (Formatação de Strings) para uma função com outra F-String.
 # EXEMPLO: Re-utilizando a função msg() que exibe uma mensagem formatada e passando uma mensagem com F-String como parâmetro.
+print('\n EX 05 - Chamando uma função com F-Strings e outras funções.')
 msg(f'Fim do Programa! Fatorial de 5 é: {fat(5)} e Fatorial de 10 é: {f"{fat(10)}"}')
 
 #=======================================================================================================================
+
+
+
+# Utilizando um "Empacotador" para receber vários parâmetros de uma função.
+# ======================================================================= #
+
+# Uma função com um parâmetro empacotador retorna uma tupla com os valores passados.
+# Para utilizar um empacotador, é só declarar a função com um * seguido da variável no parâmetro. def contador(* variavel)
+# EXEMPLO: Definindo uma função contador que recebe vários parâmetros.
+def contador(* num):
+    print(f'Foram passados {len(num)} valores para o contador.')
+    print(f'Os valores foram: {num}')
+
+
+# Como chamar uma função com um parâmetro empacotador?
+# EXEMPLO: Chamando a função contador() com os valores que serão passados dentro do parâmetro.
+print('\n EX 06 - Chamando uma função com um parâmetro empacotador.')
+contador(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+
+
+# Utilizando Listas como parâmetros.
+# ================================ #
+
+# Para utilizar listas, não precisa declarar a função com um * seguido da variável, basta utilizar a lista entre os parênteses() da função.
+# EXEMPLO: Definindo uma função que recebe uma lista como parâmetro.
+def lista(l):
+    print(f'A lista tem {len(l)} elementos.')
+    print(f'Os valores da lista são: {l}')
+
+
+
+# Como chamar uma função com uma lista como parâmetro?
+# Posso criar uma lista e passar o nome da variável ou declarar uma lista dentro de parênteses() da função.
+# EXEMPLO: Chamando a função lista() com a lista que será passada dentro do parâmetro.
+print('\n EX 07 - Chamando uma função com uma lista como parâmetro.')
+lista([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
