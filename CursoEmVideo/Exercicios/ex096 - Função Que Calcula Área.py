133d1b42d26647f1813b252ef9c64f7128a93515
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-25 15:19:23 -0300

    Update ex096 - Função Que Calcula Área.py

--- CursoEmVideo/Exercicios/ex096 - Função Que Calcula Área.py
+++ CursoEmVideo/Exercicios/ex096 - Função Que Calcula Área.py
@@ -7,17 +7,17 @@ def msg(text): # Definindo uma função de formatação com um parâmetro texto.
     print('-' * (len(text) + 10))
 
 
-def area(a=0, b=0): # Definindo uma função de cálculo de área com dois parâmetros.
-    return a * b
+def area(l=0, c=0): # Definindo uma função de cálculo de área com dois parâmetros.
+    return l * c
 
 
 msg('Controle de Terrenos') # Chamando a função msg() com o texto dentro do parâmetro.
 print()
 
-largura = float(input('Qual a largura? '))
-altura = float(input('Qual a altura? '))
+larg = float(input('Qual a largura? '))
+comp = float(input('Qual o comprimento? '))
 print()
 
 # Chamando uma função com F-Strings dentro de outra função.
-msg(f'A área de um terreno {largura:.1f} x {altura:.1f} é de {area(largura, altura):.1f} m².')
+msg(f'A área de um terreno {larg:.1f} x {comp:.1f} é de {area(larg, comp):.1f}m².')
 print()
