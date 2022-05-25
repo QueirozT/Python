3b743257b258250bdaa31f9348facc75f4293ece
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-25 16:25:11 -0300

    Create ex098 - Função de Contador.py
    Criando uma função para contar e usando o flush=True para adicionar um sleep na execução de um loop. Sem o flush, a exibição do loop só é mostrada ao final dele.

---
+++ CursoEmVideo/Exercicios/ex098 - Função de Contador.py
@@ -0,0 +1,40 @@
+from time import sleep # Para usar a função sleep em loops, é necessário habilitar o flush do print.
+
+print('===== EXERCÍCIO #098 =====')
+print()
+
+def contador(ini=0, fim=0, pas=0):
+    print('=' * 30)
+    print(f'Contando de {ini} até {fim} de {pas} em {pas}')
+
+    if ini < fim:
+        if pas < 0:
+            pas = pas * -1 # Se o passo for negativo, inverte o valor.
+        for i in range(ini, fim, pas):
+            print(i, end=' ', flush=True) # Ativando o flush para que o print seja exibido imediatamente.
+            sleep(0.5)
+        print('FIM!') 
+    elif ini > fim:
+        if pas > 0:
+            pas = pas * -1 # Se o passo for positivo, inverte o valor.
+        for i in range(ini, fim, pas):
+            print(i, end=' ', flush=True)
+            sleep(0.5)
+        print('FIM!')
+    else:
+        print('Não é possível contar.')
+    print()
+
+
+# Programa Principal.
+contador(1, 10, 1)
+
+contador(10, 0, 2)
+
+print('Agora é sua vez de personalizar a contagem!')
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+print()
+
+contador(i, f, p)
