85f0953ec2eb0e71c783866683794c42de875b7d
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-29 15:22:03 -0300

    Update ex101 - Funções Para Votação.py
    Mudando o local de importação da biblioteca para dentro da função, para poupar espaço de memória.

--- CursoEmVideo/Exercicios/ex101 - Funções Para Votação.py
+++ CursoEmVideo/Exercicios/ex101 - Funções Para Votação.py
@@ -1,10 +1,13 @@
-from datetime import date
-
 print('===== EXERCÍCIO #101 =====')
 print()
 
+
+# Definindo a função que irá retornar a idade e se pode votar ou não.
 def votar(ano):
-    idade = date.today().year - ano
+    from datetime import date # Importando a biblioteca dentro da função que vai usa-la, poupa muito espaço de memória.
+
+    idade = date.today().year - ano # Calculando a idade do usuário.
+    
     if idade < 16:
         return f'Com {idade} anos, não vota.\n'
     elif idade < 18 or idade > 65:
@@ -16,4 +19,5 @@ def votar(ano):
 # Programa Principal
 print('-' * 25)
 data = int(input('Em que ano você nasceu? '))
-print(votar(data))
+
+print(votar(data)) # Chamando a função dentro do print().
