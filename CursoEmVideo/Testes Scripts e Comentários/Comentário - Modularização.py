1661e91ce423dd0f08be501b2688840f50acf48b
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-30 17:37:22 -0300

    Create Comentário - Modularização.py
    Criando um comentário sobre como modularizar e utilizar um modulo em programas.

---
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Modularização.py
@@ -0,0 +1,19 @@
+# MODULARIZAÇÃO: É uma forma de organizar um programa, de forma que ele possa ser reutilizado em outros programas.
+
+# Todos arquivos .py que contiverem uma função podem ser importados para outros programas.
+
+# Como modularizar um programa?
+# =========================== #
+
+# Os passos para criar um módulo são:
+# 1 - Criar um arquivo de funções.
+# 2 - Importar o arquivo de funções no programa principal.
+# 3 - Chamar o import quando for utilizar as funções.
+
+
+# Como chamar um módulo?
+# ==================== #
+
+# Para chamar um módulo, basta utilizar o nome do módulo.
+# EX: import modulo
+#     modulo.funcao()
