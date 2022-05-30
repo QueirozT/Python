8114cb63addb954303193ca2ba4532a1a1d987c4
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-30 17:45:06 -0300

    Update Comentário - Modularização.py
    Adicionando as vantagens da modularização.

--- CursoEmVideo/Testes Scripts e Comentários/Comentário - Modularização.py
+++ CursoEmVideo/Testes Scripts e Comentários/Comentário - Modularização.py
@@ -11,9 +11,23 @@
 # 3 - Chamar o import quando for utilizar as funções.
 
 
-# Como chamar um módulo?
+
+# Como inportar um módulo?
 # ==================== #
 
 # Para chamar um módulo, basta utilizar o nome do módulo.
 # EX: import modulo
 #     modulo.funcao()
+
+
+
+# Quais as vantagens de modularizar um programa?
+# ============================================ #
+
+# 1 - Reduz o código / Organiza.
+# 2 - Melhorar a legibilidade do código.
+# 3 - Facilita a manutenção do código.
+# 4 - Ocultar detalhes do programa.
+# 5 - Reutilização de código.
+
+# ====================================================================================================================== #
