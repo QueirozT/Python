49d0d87737bfd4d07dd3345cefd4d1f8cf757c77
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-05-31 22:40:12 -0300

    Update ex108 - Formatando Moedas em Python.py
    Alterando o nome da função na chamada para coincidir com a alteração no modulo.

--- CursoEmVideo/Exercicios/ex108 - Formatando Moedas em Python.py
+++ CursoEmVideo/Exercicios/ex108 - Formatando Moedas em Python.py
@@ -5,7 +5,7 @@ print()
 
 v = float(input('Digite o preço: R$'))
 
-print(f'A metade de {moeda.valor(v)} é {moeda.valor(moeda.metade(v))}.')
-print(f'O dobro de {moeda.valor(v)} é {moeda.valor(moeda.dobro(v))}')
-print(f'Aumentando 10%, temos {moeda.valor(moeda.aumentar(v, 10))}')
+print(f'A metade de {moeda.moeda(v)} é {moeda.moeda(moeda.metade(v))}.')
+print(f'O dobro de {moeda.moeda(v)} é {moeda.moeda(moeda.dobro(v))}')
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(v, 10))}')
 print()
