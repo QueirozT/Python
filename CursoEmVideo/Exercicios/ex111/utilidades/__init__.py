c84000e732dede8e2453601e5b2a106e5e5a8910
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-01 22:33:04 -0300

    Create __init__.py
    Adicionando uma DefString ao pacote utilidades.

---
+++ CursoEmVideo/Exercicios/ex111/utilidades/__init__.py
@@ -0,0 +1,6 @@
+"""
+-> Este é um pacote de utilidades.
+- Este pacote contém divérsos módulos e funções de várias utilidades.
+- Um dos módulos é o "moeda" que contém funções para formatar valores monetários.
+# Criado por: Tiago.
+"""
