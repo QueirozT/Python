a0fcd6119a410f76591a07aa22e01f1998a1946f
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-01 22:34:41 -0300

    Create ex111 - Transformando Módulos em Pacotes.py
    Criando um programa para importar um módulo específico do pacote utilidades e executar suas funções retornando um valor formatado.

---
+++ CursoEmVideo/Exercicios/ex111 - Transformando Módulos em Pacotes.py
@@ -0,0 +1,9 @@
+from ex111.utilidades import moedas # Importando da pasta ex111 no pacote "utilidades" o módulo "moedas".
+
+print('===== EXERCÍCIO #110 =====')
+print()
+
+v = float(input('Digite o preço: R$'))
+
+moedas.resumo(v, 80, 35)
+print()
