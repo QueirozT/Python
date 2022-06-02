5af4539218069076a1ccb35540e22cacc2d6471a
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-01 23:04:45 -0300

    Update __init__.py
    Criando uma DefString para a função leiaDinheiro e adicionando uma função para ler e validar um número inteiro no módulo dados do pacote utilidades.

--- CursoEmVideo/Exercicios/ex112/utilidades/dados/__init__.py
+++ CursoEmVideo/Exercicios/ex112/utilidades/dados/__init__.py
@@ -1,4 +1,10 @@
 def leiaDinheiro(txt):
+    """
+    -> Função para ler e validar um valor monetário.
+    :txt = Texto a ser exibido para o usuário.
+    :return: Valor monetário.
+    # Criado por: Tiago.
+    """
     while True:
         valor = str(input(txt)).replace(',','.').strip()
 
@@ -7,3 +13,19 @@ def leiaDinheiro(txt):
         else:
             return float(valor)
             break
+
+
+def lerInt(txt):
+    """
+    -> Função para ler e validar um número inteiro.
+    :txt = Texto a ser exibido para o usuário.
+    :return: Valor inteiro.
+    # Criado por: Tiago.
+    """
+    while True:
+        num = input(txt)
+        if num.isnumeric():
+            break
+        print('\033[31mERRO! Digite um número válido!\033[m')
+
+    return int(num)
