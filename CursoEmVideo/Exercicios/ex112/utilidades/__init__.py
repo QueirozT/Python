1940bb69ac7c82a0bb29233b658338d49a1aacc5
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-01 22:48:11 -0300

    Create __init__.py
    Acrescentando uma descrição sobre o módulo dados a DefString do pacote utilidades

---
+++ CursoEmVideo/Exercicios/ex112/utilidades/__init__.py
@@ -0,0 +1,7 @@
+"""
+-> Este é um pacote de utilidades.
+- Este pacote contém divérsos módulos e funções de várias utilidades.
+- Um dos módulos é o "moeda" que contém funções para formatar valores monetários.
+- Outro módulo é o "dados" que contém funções para validar dados.
+# Criado por: Tiago.
+"""
