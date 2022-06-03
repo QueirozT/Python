d2ff057f41a7a012e1c7a28c33e8ab68deb501d4
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-03 12:04:50 -0300

    Update ex114 - O Site Está Acessível.py
    Adicionando Comentários para facilitar o entendimento do código do programa.

--- CursoEmVideo/Exercicios/ex114 - O Site Está Acessível.py
+++ CursoEmVideo/Exercicios/ex114 - O Site Está Acessível.py
@@ -1,12 +1,12 @@
-import urllib.request
+import urllib.request # Importando o módulo request da biblioteca urllib.
 
 print('===== EXERCÍCIO #114 =====')
 print()
 
 try:
-    acesso = urllib.request.urlopen('http://www.google.com')
-except urllib.error.URLError:
+    site = urllib.request.urlopen('http://www.google.com') # Tentando abrir o site através da função urlopen().
+except urllib.error.URLError: # Verificando se ocorreu algum erro.
     print('O site não está acessível no momento.')
 else:
-    print('O site está acessível.')
-print()
+    print('O site está acessível.\n')
+    print(site.read()) # Exibe todo o conteúdo do site (HTML).
