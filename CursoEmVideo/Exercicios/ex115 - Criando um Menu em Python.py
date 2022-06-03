7752e6676a9f7716c3bcc3f879ae8b8c1ed1505d
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-03 16:09:59 -0300

    Update ex115 - Criando um Menu em Python.py
    Importando o módulo arquivo e adicionando ao programa as funções para verificar se um arquivo existe e criar um arquivo novo com o local onde o arquivo deve ser salvo.

--- CursoEmVideo/Exercicios/ex115 - Criando um Menu em Python.py
+++ CursoEmVideo/Exercicios/ex115 - Criando um Menu em Python.py
@@ -1,8 +1,16 @@
-from ex115.lib.interface import * # Utilizando o * para importar todas as funções da biblioteca.
+from ex115.lib.interface import * # Utilizando o * para importar todos os módulos da biblioteca.
+from ex115.lib.arquivo import * # Importando o módulo "arquivo" que verifica se o arquivo existe ou cria um.
 from time import sleep # Importando o módulo sleep da biblioteca time.
 
 print('===== EXERCÍCIO #115 =====')
 print()
+
+nome = 'Arquivo de Dados.txt' # Adicionando o nome do arquivo a variável.
+arq = f".\CursoEmVideo\Exercicios\ex115\lib\{nome}" # Adicionando o caminho do arquivo a variável.
+
+if not arquivoExiste(arq): # Verificando se o arquivo não existe.
+    criarArquivo(arq) # Criando o arquivo.
+
 
 while True:
 # Criando um menu através da função menu() e retornando o valor da opção escolhida pelo usuário.
