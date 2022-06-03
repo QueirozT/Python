40fbe06e5f7904cc5ba0afeaa0188b175fc39b97
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-03 17:07:39 -0300

    Update __init__.py
    Adicionando uma função para cadastrar novos dados em um arquivo e modificando a função de leitura para separar os dados de um arquivo formatar e exibir.

--- CursoEmVideo/Exercicios/ex115/lib/arquivo/__init__.py
+++ CursoEmVideo/Exercicios/ex115/lib/arquivo/__init__.py
@@ -16,14 +16,15 @@ def arquivoExiste(nome):
         return True
 
 
-def criarArquivo(nome):
+def criarArquivo(nome, local):
     """
     -> Função que tenta abrir um arquivo, e caso ele não exista cria um.
     :nome = nome do arquivo.
+    :local = caminho do arquivo.
     :return: um arquivo novo.
     """
     try:
-        a = open(nome, 'wt+') # Tentando abrir um arquivo de texto com os parâmetros "wt" Write Text e caso não exista cria um "+".
+        a = open(local, 'wt+') # Tentando abrir um arquivo de texto com os parâmetros "wt" Write Text e caso não exista cria um "+".
         a.close()
     except:
         print('Houve um erro ao criar o arquivo.')
@@ -43,6 +44,35 @@ def lerArquivo(nome):
         print('Falha ao ler o arquivo.')
     else:
         msg('PESSOAS CADASTRADAS')
-        print(a.read()) # Mostrando tudo que está dentro do arquivo.
+        for lin in a: # Lendo o arquivo linha por linha.
+            pessoa = lin.strip().split(';') # Separando o nome e a idade e armazenando em uma lista.
+            pessoa[1].replace('\n', '') # Substituindo a quebra de linha "\n" por nada para tirar os espaços na hora da leitura.
+            print(f'{"":>5}{pessoa[0]:<25}{pessoa[1]:>20} anos') # Mostrando o nome e idade da pessoa formatados com espaços.
     finally:
         a.close() # fechando o arquivo.
+
+
+def cadastrar(arq, nome='', idade=0):
+    """
+    -> Função que recebe o local do arquivo um nome e uma idade e adiciona os dados ao arquivo.
+    :arq = local do arquivo.
+    :nome = nome da pessoa.
+    :idade = idade da pessoa.
+    :return: não há retorno.
+    """
+    
+    if nome == '':
+        nome = 'Desconhecido'
+    
+    try:
+        a = open(arq, 'at') # Tentando abrir um arquivo de texto com os parâmetros "at" Append Text.
+    except:
+        print('Falha ao cadastrar a pessoa.')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n') # Escrevendo o nome e idade na ultima linha do arquivo e dando uma quebra de linha.
+        except:
+            print('Falha ao escrever o arquivo.')
+        else:
+            print(f'{cor(3)}Novo registro {nome} adicionado com sucesso!{cor()}')
+            a.close()
