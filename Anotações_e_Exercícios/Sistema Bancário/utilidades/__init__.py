d1afe149b219036cef953527f2a0fdab1cf770e7
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-18 12:19:04 -0300

    Update __init__.py

--- Anotações_e_Exercícios/Sistema Bancário/utilidades/__init__.py
+++ Anotações_e_Exercícios/Sistema Bancário/utilidades/__init__.py
@@ -1,6 +1,6 @@
 """
 Este Pacote utilidades contém os módulos do programa e um Pacote com as classes.
 - BancoDeDados = Módulo com funções de um "BancoDeDados".
-- dados = Módulo com funções que coletam, validam ou modificam dados do programa.
+- dados = Módulo com funções de formatação e tratamento de dados.
 - classes = Classes utilizadas no programa.
 """
