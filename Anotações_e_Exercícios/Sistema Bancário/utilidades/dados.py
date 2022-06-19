ca211a0e4863959be2f5cbe01242784c501156e6
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-19 11:41:40 -0300

    Aplicando um padrão as DocStrings
    Modificando as DocStrings do módulo dados e das funções para aplicar um padrão.

--- Anotações_e_Exercícios/Sistema Bancário/utilidades/dados.py
+++ Anotações_e_Exercícios/Sistema Bancário/utilidades/dados.py
@@ -1,9 +1,25 @@
+"""
+Módulo responsável pela formatação e entrada de dados do programa.
+
+Este módulo contém uma função para validar a entrada de dados, para garantir que o usuário irá informar os valores corretamente. e para formatar a saída de dados.
+"""
+
 def msg(txt, tam=None, titulo=False):
     """
     Função que exibe uma mensagem formatada na tela.
-    - txt: Texto a ser exibido.
-    - tam: (Opcional) Tamanho maximo das linhas no texto.
-    - titulo: (Opcional) Se True, exibe o texto como título, sem as linhas de baixo.
+
+    Esta função recebe uma mensagem e a formata para ser exibida entre linhas. também recebe parâmetros opcionais para definir o tamanho das linhas ou remover uma das linhas.
+        
+        Parametros:
+            txt (str):
+                Texto a ser exibido.
+            tam (int, None) - Opcional:
+                Tamanho maximo das linhas no texto.
+            titulo (bool) - Opcional: 
+                True = exibe o texto como título (sem a linha de baixo).
+        
+        Retorno:
+            None: A mensagem formatada será exibida no momento em que a função for chamada.
     """
     l = tam if tam else len(txt) + 10
     
@@ -15,9 +31,17 @@ def msg(txt, tam=None, titulo=False):
 def leitura(txt, max=None):
     """
     Função que valida a entrada de dados.
-    - txt: Texto a ser exibido para o usuário.
-    - max: (Opcional) valor máximo aceitavel.
-    - return: Valor digitado pelo usuário.
+
+    Esta função recebe uma mensagem que é exibida enquanto aguarda e valida a entrada de dados. após validar, retorna o valor validado. Ela também recebe um parâmetro opcional que informa o valor máximo que o usuário pode informar.
+
+        Parametros:
+            txt (str):
+                Texto a ser exibido para o usuário.
+            max (int, None) - Opcional:
+                valor máximo aceitavel.
+        
+        Retorno:
+            val (float): Valor válido digitado pelo usuário.
     """
     while True:
         try:
