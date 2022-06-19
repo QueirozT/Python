e076d82380530a0918578489720783119c3421b4
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-19 13:28:46 -0300

    Adicionando DocStrings e Type Hint

--- Anotações_e_Exercícios/Sistema Bancário/utilidades/classes/cp.py
+++ Anotações_e_Exercícios/Sistema Bancário/utilidades/classes/cp.py
@@ -3,6 +3,8 @@ from utilidades.classes.conta import Conta  # A importação usa a perspectiva d
 
 class ContaPoupanca(Conta):
     """
+    Classe Conta Poupança que herda de Conta
+
     Esta classe representa uma conta poupança que herda da classe abstrata Conta.
     - Esta classe recebe:
         - O construtor da classe Conta.
@@ -10,8 +12,25 @@ class ContaPoupanca(Conta):
         - O método abstrato sacar da classe Conta.
     - Esta classe sobrescreve:
         - O método abstrato sacar da classe Conta.
+
+    Métodos
+    -------
+    sacar(valor)
+        Subtrai valores do saldo da conta.
     """
-    def sacar(self, valor):  # Sobrescrevendo o método abstrato sacar da classe Conta.
+    def sacar(self, valor: float) -> None:  # Sobrescrevendo o método abstrato sacar da classe Conta.
+        """
+        Este método sobrescreve o método abstrato da class Conta.
+
+        Parametros
+        ----------
+            valor : float
+                Valor a subtrair
+        
+        Retorno
+        -------
+            None
+        """
         if self._saldo < valor: 
             raise ValueError('Saldo insuficiente.')  # Lançando uma excessão caso o saldo seja insuficiente.
         
