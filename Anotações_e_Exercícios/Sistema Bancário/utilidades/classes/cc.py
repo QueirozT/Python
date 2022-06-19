d66c83d26df9f7f50ca709ba5b17bed73977b768
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-19 13:25:11 -0300

    Adicionando DocStrings e Type Hint

--- Anotações_e_Exercícios/Sistema Bancário/utilidades/classes/cc.py
+++ Anotações_e_Exercícios/Sistema Bancário/utilidades/classes/cc.py
@@ -3,6 +3,8 @@ from utilidades.classes.conta import Conta
 
 class ContaCorrente(Conta):
     """
+    Classe Conta Corrente que herda de Conta
+
     Esta classe representa uma conta corrente que herda da classe abstrata Conta.
     - Esta classe recebe:
         - O construtor da classe Conta.
@@ -13,16 +15,51 @@ class ContaCorrente(Conta):
             - Adicionando um parâmetro limite.
         - O método sacar da classe Conta.
             - Adicionando a verificação do saldo com o limite.
+    
+    Atributos
+    ---------
+    agencia : str
+        chave da agência
+    conta : str
+        chave da conta
+    saldo : float
+        saldo da conta
+    limite : int
+        limite da conta (padrão 100)
+
+    Métodos
+    -------
+    limite(getter)
+        Coleta a chave da agência
+    sacar(valor)
+        Subtrai valores do saldo da conta.
     """
-    def __init__(self, agencia, conta, saldo, limite=100):  # Sobrescrevendo o método __init__ da classe Conta.
+    def __init__(self, agencia: str, conta: str, saldo: float, limite: int=100):  # Sobrescrevendo o método __init__ da classe Conta.
+        """
+        Sobrescreve o inicializador da Conta
+
+        Este inicializador sobrescreve o inicializador da Abstract Class Conta e insere o atributo limite
+        """
         super().__init__(agencia, conta, saldo)  # Passando os parâmetros para o método __init__ da classe Conta através do super().
         self._limite = limite
 
     @property
-    def limite(self):
+    def limite(self) -> int:
         return self._limite
 
-    def sacar(self, valor):
+    def sacar(self, valor: float) -> None:
+        """
+        Este método sobrescreve o método abstrato da class Conta.
+
+        Parametros
+        ----------
+            valor : float
+                Valor a subtrair
+        
+        Retorno
+        -------
+            None
+        """
         if (self._saldo + self._limite) < valor:
             raise ValueError('Saldo insuficiente.')
         
