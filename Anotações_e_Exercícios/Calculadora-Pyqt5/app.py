2739ee654f3b5bf0319b8ced66830755e93e92a1
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-06-29 14:09:29 -0300

    Update app.py
    Adicionando uma DocString no programa principal da calculadora.

--- Anotações_e_Exercícios/Calculadora-Pyqt5/app.py
+++ Anotações_e_Exercícios/Calculadora-Pyqt5/app.py
@@ -1,3 +1,19 @@
+"""
+Programa que gera uma calculadora simples
+
+Este programa gera uma calculadora usando a biblioteca pyqt5 que está instalada no próprio ambiente virtual .env
+Este é um programa simples, criado apenas para demonstração e futuras consultas.
+
+Modulos Importados:
+
+    sys: É uma bibliteca que coleta e interage com informações do sistema.
+        Esta sendo usada para coletar os inputs do usuário para o Pyqt5
+
+    PyQt5: Biblioteca responsável por criar a calculadora.
+        Está sendo usada para criar o display que irá exibir os gráficos gerados pela class Calculadora e coletar os inputs.
+
+    main: É o módulo que contém a class e todos os métodos importados do PyQt5 que irão gerar a calculadora.
+"""
 import sys
 from PyQt5.QtWidgets import QApplication
 from main import Calculadora
